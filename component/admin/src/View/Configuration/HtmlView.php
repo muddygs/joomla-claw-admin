4294fb6d1679d679a7231fe5302a6a92e78ff7aa
maddog <muddygs@gmail.com>
2023-02-08 07:58:09 -0500

    Starting new overall configuration control panel.

---
+++ component/admin/src/View/Configuration/HtmlView.php
@@ -0,0 +1,149 @@
+<?php
+/**
+ * @package     ClawCorp
+ * @subpackage  com_claw
+ *
+ * @copyright   (C) 2022 C.L.A.W. Corp. All Rights Reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+namespace ClawCorp\Component\Claw\Administrator\View\Configuration;
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
+use Joomla\CMS\MVC\View\GenericDataException;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Toolbar\Toolbar;
+use Joomla\CMS\Toolbar\ToolbarHelper;
+
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_claw
+ *
+ * @copyright   Copyright (C) 2020 John Smith. All rights reserved.
+ * @license     GNU General Public License version 3; see LICENSE
+ */
+
+/**
+ * Main "Hello World" Admin View
+ */
+class HtmlView extends BaseHtmlView
+{
+	/**
+	 * The search tools form
+	 *
+	 * @var    Form
+	 * @since  1.6
+	 */
+	public $filterForm;
+
+	/**
+	 * The active search filters
+	 *
+	 * @var    array
+	 * @since  1.6
+	 */
+	public $activeFilters = [];
+
+	/**
+	 * Category data
+	 *
+	 * @var    array
+	 * @since  1.6
+	 */
+	protected $categories = [];
+
+	/**
+	 * An array of items
+	 *
+	 * @var    array
+	 * @since  1.6
+	 */
+	protected $items = [];
+
+	/**
+	 * The pagination object
+	 *
+	 * @var    Pagination
+	 * @since  1.6
+	 */
+	protected $pagination;
+
+	/**
+	 * The model state
+	 *
+	 * @var    Registry
+	 * @since  1.6
+	 */
+	protected $state;
+
+	/**
+	 * @param   string  $tpl  The name of the template file to parse; automatically searches through the template paths.
+	 * @return  void
+	 */
+	function display($tpl = null)
+	{
+		$this->form  = $this->get('Form');
+		$this->item  = $this->get('Item');
+		$this->state = $this->get('State');
+
+		// Check for errors.
+		if (count($errors = $this->get('Errors'))) {
+			throw new GenericDataException(implode("\n", $errors), 500);
+		}
+
+		$this->addToolbar();
+
+		parent::display($tpl);
+	}
+
+		/**
+	 * Add the page title and toolbar.
+	 *
+	 * @return  void
+	 *
+	 * @throws \Exception
+	 * @since   1.6
+	 */
+	protected function addToolbar()
+	{
+		Factory::getApplication()->input->set('hidemainmenu', true);
+		$isNew      = ($this->item->id == 0);
+		$user  = Factory::getApplication()->getIdentity();
+
+		// $canDo = ContentHelper::getActions('com_countrybase');
+
+		$toolbar = Toolbar::getInstance();
+		// TODO: Update to Joomla 5 API...no clue how to implement the Interface yet
+		// $IToolbar = new ToolbarFactoryInterface();
+		// $toolbar = $IToolbar->createToolbar();
+
+		ToolbarHelper::title(
+			'CLAW Configuration ' . ($isNew ? 'Add' : 'Edit')
+		);
+
+		if (true /*$canDo->get('core.create')*/)
+		{
+			if ($isNew)
+			{
+				$toolbar->apply('currentitem.save');
+			}
+			else
+			{
+				$toolbar->apply('currentitem.apply');
+			}
+			$toolbar->save('currentitem.save');
+
+		}
+
+		$toolbar->cancel('currentitem.cancel', 'JTOOLBAR_CLOSE');
+
+		if ($user->authorise('core.admin', 'com_claw') || $user->authorise('core.options', 'com_claw'))
+		{
+			$toolbar->preferences('com_claw');
+		}
+
+	}
+
+}
