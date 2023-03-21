c7fa1299fc995ef0257b54715e8b69709ddb8329
muddygs <muddygs@gmail.com>
2023-03-20 22:53:18 -0400

    Cleanup; bump (c)

--- component/admin/services/provider.php
+++ component/admin/services/provider.php
@@ -3,7 +3,7 @@
  * @package     ClawCorp
  * @subpackage  com_claw
  *
- * @copyright   (C) 2022 C.L.A.W. Corp. All Rights Reserved.
+ * @copyright   (C) 2023 C.L.A.W. Corp. All Rights Reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
@@ -12,8 +12,11 @@ defined('_JEXEC') or die;
 use Joomla\CMS\Dispatcher\ComponentDispatcherFactoryInterface;
 use Joomla\CMS\Extension\ComponentInterface;
 use Joomla\CMS\Extension\MVCComponent;
+use Joomla\CMS\Extension\Service\Provider\CategoryFactory;
 use Joomla\CMS\Extension\Service\Provider\ComponentDispatcherFactory;
 use Joomla\CMS\Extension\Service\Provider\MVCFactory;
+use Joomla\CMS\Extension\Service\Provider\RouterFactory;
+
 use Joomla\CMS\MVC\Factory\MVCFactoryInterface;
 use Joomla\DI\Container;
 use Joomla\DI\ServiceProviderInterface;
@@ -27,8 +30,13 @@ return new class implements ServiceProviderInterface {
 			throw new RuntimeException('CLAW library is not installed', 500);
 		}
 
+        
+        
+        $container->registerServiceProvider(new CategoryFactory('\\ClawCorp\\Component\\Claw'));
         $container->registerServiceProvider(new MVCFactory('\\ClawCorp\\Component\\Claw'));
         $container->registerServiceProvider(new ComponentDispatcherFactory('\\ClawCorp\\Component\\Claw'));
+        $container->registerServiceProvider(new RouterFactory('\\ClawCorp\\Component\\Claw'));
+
         $container->set(
             ComponentInterface::class,
             function (Container $container) {
