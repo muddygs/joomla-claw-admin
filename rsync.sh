5f6035e95220171a6b20affab28f5bdeef0c2f7b
muddygs <muddygs@gmail.com>
2023-03-20 22:53:42 -0400

    Adding site component to manifest

--- rsync.sh
+++ rsync.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #rsync -arv --delete /var/www/eb/public_html/administrator/components/com_claw/ component/admin/
 rsync -arv /var/www/eb/public_html/administrator/components/com_claw/ component/admin/
+rsync -arv /var/www/eb/public_html/components/com_claw/ component/site/
 mv component/admin/claw.xml component/
 mv component/admin/script.php component/
 #rsync -arv --delete /var/www/eb/public_html/libraries/claw/ library/
