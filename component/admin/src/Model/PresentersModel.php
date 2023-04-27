505e4e8f5b5de2d78476be251a9f684985827eb9
muddygs <muddygs@gmail.com>
2023-04-26 22:19:45 -0400

    Implemented event filter for presenters.

--- component/admin/src/Model/PresentersModel.php
+++ component/admin/src/Model/PresentersModel.php
@@ -11,6 +11,7 @@ namespace ClawCorp\Component\Claw\Administrator\Model;
 
 defined('_JEXEC') or die;
 
+use ClawCorpLib\Lib\Aliases;
 use Joomla\CMS\Factory;
 use Joomla\CMS\MVC\Model\ListModel;
 
@@ -22,135 +23,152 @@ use Joomla\CMS\MVC\Model\ListModel;
 class PresentersModel extends ListModel
 {
 
-	private array $list_fields = [
-		'id',
-		'published',
-		'name',
-		'legal_name',
-		'event',
-		'email',
-		'phone',
-		'arrival',
-		'photo',
-		'mtime',
-		'submission_date'
-	];	
+  private array $list_fields = [
+    'id',
+    'published',
+    'name',
+    'legal_name',
+    'event',
+    'email',
+    'phone',
+    'arrival',
+    'photo',
+    'mtime',
+    'submission_date'
+  ];	
 
-	/**
-	 * Constructor.
-	 *
-	 * @param   array  $config  An optional associative array of configuration settings.
-	 *
-	 * @see     \JController
-	 * @since   1.6
-	 */
-	public function __construct($config = array())
-	{
-		if (empty($config['filter_fields'])) {
-			$config['filter_fields'] = [];
-			
-			foreach( $this->list_fields AS $f )
-			{
-				$config['filter_fields'][] = $f;
-				$config['filter_fields'][] = 'a.'.$f;
-			}
-		}
+  /**
+   * Constructor.
+   *
+   * @param   array  $config  An optional associative array of configuration settings.
+   *
+   * @see     \JController
+   * @since   1.6
+   */
+  public function __construct($config = array())
+  {
+    if (empty($config['filter_fields'])) {
+      $config['filter_fields'] = [];
+      
+      foreach( $this->list_fields AS $f )
+      {
+        $config['filter_fields'][] = $f;
+        $config['filter_fields'][] = 'a.'.$f;
+      }
+    }
 
-		parent::__construct($config);
-	}
+    parent::__construct($config);
+  }
 
-	/**
-	 * Method to auto-populate the model state.
-	 *
-	 * This method should only be called once per instantiation and is designed
-	 * to be called on the first call to the getState() method unless the model
-	 * configuration flag to ignore the request is set.
-	 *
-	 * Note. Calling getState in this method will result in recursion.
-	 *
-	 * @param   string  $ordering   An optional ordering field.
-	 * @param   string  $direction  An optional direction (asc|desc).
-	 *
-	 * @return  void
-	 *
-	 * @since   3.0.1
-	 */
-	protected function populateState($ordering = 'name', $direction = 'ASC')
-	{
-		$app = Factory::getApplication();
+  /**
+   * Method to auto-populate the model state.
+   *
+   * This method should only be called once per instantiation and is designed
+   * to be called on the first call to the getState() method unless the model
+   * configuration flag to ignore the request is set.
+   *
+   * Note. Calling getState in this method will result in recursion.
+   *
+   * @param   string  $ordering   An optional ordering field.
+   * @param   string  $direction  An optional direction (asc|desc).
+   *
+   * @return  void
+   *
+   * @since   3.0.1
+   */
+  protected function populateState($ordering = 'name', $direction = 'ASC')
+  {
+    $app = Factory::getApplication();
 
-		// List state information
-		$value = $app->input->get('limit', $app->get('list_limit', 0), 'uint');
-		$this->setState('list.limit', $value);
+    // List state information
+    $value = $app->input->get('limit', $app->get('list_limit', 0), 'uint');
+    $this->setState('list.limit', $value);
 
-		$value = $app->input->get('limitstart', 0, 'uint');
-		$this->setState('list.start', $value);
+    $value = $app->input->get('limitstart', 0, 'uint');
+    $this->setState('list.start', $value);
 
-		$search = $this->getUserStateFromRequest($this->context . '.filter.search', 'filter_search');
-		$this->setState('filter.search', $search);
+    $search = $this->getUserStateFromRequest($this->context . '.filter.search', 'filter_search');
+    $this->setState('filter.search', $search);
 
-		// List state information.
-		parent::populateState($ordering, $direction);
-	}
+    // List state information.
+    parent::populateState($ordering, $direction);
+  }
 
-	/**
-	 * Method to get a store id based on model configuration state.
-	 *
-	 * This is necessary because the model is used by the component and
-	 * different modules that might need different sets of data or different
-	 * ordering requirements.
-	 *
-	 * @param   string  $id  A prefix for the store id.
-	 *
-	 * @return  string  A store id.
-	 *
-	 * @since   1.6
-	 */
-	protected function getStoreId($id = '')
-	{
-		// Compile the store id.
-		$id .= ':' . serialize($this->getState('filter.name'));
-		$id .= ':' . $this->getState('filter.search');
-		$id .= ':' . $this->getState('filter.state');
+  /**
+   * Method to get a store id based on model configuration state.
+   *
+   * This is necessary because the model is used by the component and
+   * different modules that might need different sets of data or different
+   * ordering requirements.
+   *
+   * @param   string  $id  A prefix for the store id.
+   *
+   * @return  string  A store id.
+   *
+   * @since   1.6
+   */
+  protected function getStoreId($id = '')
+  {
+    // Compile the store id.
+    $id .= ':' . serialize($this->getState('filter.name'));
+    $id .= ':' . $this->getState('filter.search');
+    $id .= ':' . $this->getState('filter.state');
 
-		return parent::getStoreId($id);
-	}
+    return parent::getStoreId($id);
+  }
 
-	/**
-	 * Get the master query for retrieving a list of countries subject to the model state.
-	 *
-	 * @return  \Joomla\Database\DatabaseQuery
-	 *
-	 * @since   1.6
-	 */
-	protected function getListQuery()
-	{
-		// Create a new query object.
-		$db    = $this->getDatabase();
-		$query = $db->getQuery(true);
+  /**
+   * Get the master query for retrieving a list of countries subject to the model state.
+   *
+   * @return  \Joomla\Database\DatabaseQuery
+   *
+   * @since   1.6
+   */
+  protected function getListQuery()
+  {
+    // Create a new query object.
+    $db    = $this->getDatabase();
+    $query = $db->getQuery(true);
 
-		// Select the required fields from the table.
-		$query->select(
-			$this->getState(
-				'list.select', array_map( function($a) use($db) { return $db->quoteName('a.'.$a); }, $this->list_fields)
-			)
-		)
-			->from($db->quoteName('#__claw_presenters', 'a'));
+    // Select the required fields from the table.
+    $query->select(
+      $this->getState(
+        'list.select', array_map( function($a) use($db) { return $db->quoteName('a.'.$a); }, $this->list_fields)
+      )
+    )
+      ->from($db->quoteName('#__claw_presenters', 'a'));
 
-		// Filter by search in title.
-		$search = $this->getState('filter.search');
+    // Filter by search in title.
+    $search = $this->getState('filter.search');
 
-		if (!empty($search))
-		{
-			$search = $db->quote('%' . str_replace(' ', '%', $db->escape(trim($search), true) . '%'));
-			$query->where('(a.value LIKE ' . $search . ')');
-		}
+    if (!empty($search))
+    {
+      $search = $db->quote('%' . str_replace(' ', '%', $db->escape(trim($search), true) . '%'));
+      $query->where('(a.value LIKE ' . $search . ')');
+    }
 
-		// Add the list ordering clause.
-		$orderCol  = $this->state->get('list.ordering', 'a.name');
-		$orderDirn = $this->state->get('list.direction', 'ASC');
+    $event = $this->getState('filter.event');
 
-		$query->order($db->escape($orderCol) . ' ' . $db->escape($orderDirn));
-		return $query;
-	}
+    switch ($event) {
+      case '':
+      case '_current_':
+        $event = Aliases::current;
+        break;
+      case '_all_':
+        $event = '';
+    }
+    
+    if ( $event != '' )
+    {
+      $query->where('a.event = :event')
+      ->bind(':event', $event);
+    }
+
+    // Add the list ordering clause.
+    $orderCol  = $this->state->get('list.ordering', 'a.name');
+    $orderDirn = $this->state->get('list.direction', 'ASC');
+
+    $query->order($db->escape($orderCol) . ' ' . $db->escape($orderDirn));
+    return $query;
+  }
 }
