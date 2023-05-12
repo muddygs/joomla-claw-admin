3824cbc354a13a88d91b9ada8b8a48e302c1ddcc
muddygs <muddygs@gmail.com>
2023-05-12 07:46:54 -0400

    Migrating in bootstrap 5 helper functions.

---
+++ library/Helpers/Bootstrap.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace ClawCorpLib\Helpers;
+
+use ClawCorpLib\Lib\Aliases;
+use Joomla\CMS\User\UserHelper;
+
+class Bootstrap
+{
+  static function getSponsorTag($sponsorImg, $sponsorName, $sponsorLink)
+  {
+    $tag = '';
+
+    $link = empty($sponsorLink) ? '' : $sponsorLink;
+
+    if (!empty($link)) {
+      $tag = "<a href=\"$link\" alt=\"$sponsorName\" title=\"$sponsorName\" target=\"_blank\">";
+    }
+
+    $img = Aliases::imagedir . $sponsorImg;
+    $tag = $tag . "<img src=\"$img\" class=\"img-fluid mx-auto\"/>";
+    if (!empty($link)) {
+      $tag = $tag . '</a>';
+    }
+
+    return $tag;
+  }
+
+  /**
+   * Writes out HTML for pilled tabs in Bootstrap 5
+   * @return Nothing
+   */
+  static function writePillTabs(array $tabTitles, array $tabContent, string $activeTab = '')
+  {
+    $guid = UserHelper::genRandomPassword(8);
+    if ($activeTab == '') $activeTab = $tabTitles[0];
+?>
+    <ul class="nav nav-pills nav-fill mb-3" id="pills-tab-<?php echo $guid ?>" role="tablist">
+      <?php
+      foreach ($tabTitles as $title) {
+        $active = $title == $activeTab ? 'active' : '';
+        $aria = $title == $activeTab ? 'true' : 'false';
+        $tabName = strtolower($title);
+        $tabName = preg_replace("/[^\w]/", '', $tabName);
+      ?>
+        <li class="nav-item" role="presentation">
+          <button class="nav-link <?php echo $active ?>" id="pills-<?php echo $tabName ?>-tab" data-bs-toggle="pill" data-bs-target="#pills-<?php echo $tabName ?>" role="tab" aria-controls="pills-<?php echo $tabName ?>" aria-selected="<?php echo $aria ?>"><?php echo $title ?></button>
+        </li>
+      <?php
+      }
+      ?>
+    </ul>
+    <div class="tab-content" id="pills-tab-<?php echo $guid ?>Content">
+      <?php
+      reset($tabContent);
+
+      foreach ($tabTitles as $title) {
+        $active = $title == $activeTab ? 'show active' : '';
+        $tabName = strtolower($title);
+        $tabName = preg_replace("/[^\w]/", '', $tabName);
+
+      ?>
+        <div class="tab-pane fade <?php echo $active ?>" id="pills-<?php echo $tabName ?>" role="tabpanel" aria-labelledby="pills-<?php echo $tabName ?>-tab">
+          <?php
+          echo current($tabContent);
+          next($tabContent);
+          ?>
+        </div>
+      <?php
+      }
+      ?>
+    </div>
+<?php
+  }
+
+
+  /**
+   * Generates HTML based on Bootstrap 5 flex. Input is array with icon and content.
+   */
+  static function writeGrid(array $content, array $tags = [], bool $asString = false)
+  {
+    $result = '<div class="container px-2 py-2" id="icon-grid-vip">
+    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-2 px-4 py-2 justify-content-center">';
+
+    $tagSize = count($tags);
+
+    foreach ($content as $icon => $content) {
+      $result .= '<div class="col d-flex align-items-start">';
+      $result .= '<div class="flex-grow-0"><span style="color:#ffae00; width:2.5em" class="py-2 py-md-0 text-center fa fa-2x fa-' . $icon . '"></span></div>';
+      $result .= '<div class="py-2 py-md-0">';
+      foreach ($content as $i => $t) {
+        if ($i < $tagSize) $result .= $tags[$i][0];
+        $result .= $t;
+        if ($i < $tagSize) $result .= $tags[$i][1];
+      }
+      $result .= '</div>';
+      $result .= '</div>';
+    }
+
+    $result .= '</div></div>';
+
+    if ($asString) return $result;
+    echo $result;
+  }
+}
