39b6f6fdf02dd1094121e165441b622ef23ee359
muddygs <muddygs@gmail.com>
2023-05-12 07:41:15 -0400

    Auto-reformat Handles duplication of older submissions for the current CLAW event.

--- component/site/src/Model/SkillsubmissionModel.php
+++ component/site/src/Model/SkillsubmissionModel.php
@@ -15,56 +15,136 @@ defined('_JEXEC') or die;
 use Joomla\CMS\MVC\Model\AdminModel;
 use Joomla\CMS\Language\Text;
 use ClawCorpLib\Helpers\Helpers;
+use ClawCorpLib\Lib\Aliases;
+use ClawCorpLib\Lib\ClawEvents;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Router\Route;
+use Joomla\Database\Exception\DatabaseNotFoundException;
 
 /**
  * Get a single presenter submission from an authenticated user
  */
 class SkillsubmissionModel extends AdminModel
 {
-	public function getForm($data = [], $loadData = true)
-	{
-		// Get the form.
-		$form = $this->loadForm('com_claw.skillsubmission', 'skillsubmission', array('control' => 'jform', 'load_data' => $loadData));
+  public function getForm($data = [], $loadData = true)
+  {
+    // Get the form.
+    $form = $this->loadForm(
+      'com_claw.skillsubmission',
+      'skillsubmission',
+      [
+        'control' => 'jform',
+        'load_data' => $loadData
+      ]
+    );
 
-		if (empty($form)) {
-			return false;
-		}
+    if (empty($form)) {
+      return false;
+    }
 
-		return $form;
-	}
+    return $form;
+  }
 
-	protected function loadFormData()
-	{
-		$mergeData = null;
-		$submittedFormData = Helpers::sessionGet('formdata');
-		if ( $submittedFormData ) {
-			$mergeData = json_decode($submittedFormData, true);
-		}
+  /**
+   * Given the ID of a record, attempt to copy it to the current
+   * event
+   * @param int $id 
+   * @return int|false 
+   * @throws DatabaseNotFoundException 
+   */
+  public function duplicate(int $id)
+  {
+    /** @var \Joomla\CMS\Application\CMSApplicationInterface */
+    $app = Factory::getApplication();
+    $db = $this->getDatabase();
+    $uid = $app->getIdentity()->id;
 
-		$data = $this->getItem();
+    $query = $db->getQuery(true);
+    $query->select('*')
+      ->from('#__claw_skills')
+      ->where('id = :id')
+      ->bind(':id', $id);
 
-		if ( $mergeData ) {
-			foreach ( $mergeData AS $key => $value ) {
-				$data->$key = $value;
-			}
-		}
+    $db->setQuery($query);
+    $record = $db->loadAssoc();
 
-		if ( $data->photo ) {
-			Helpers::sessionSet('photo', $data->photo);
-		}
+    $success = false;
 
- 		return $data;
-	}
+    if ( $record != null ) {
+      if ( $record['owner'] == $uid && $record['event'] != Aliases::current ) {
+        $record['id'] = 0;
+        $record['event'] = Aliases::current;
+        $record['published'] = 3;
+        $record['day'] = $db->getNullDate();
+        $record['mtime'] = Helpers::mtime();
+        $record['submission_date'] = date("Y-m-d");
+        $record['presenters'] = '';
+        $record['location'] = 2147483647;
+        $record['handout_id'] = 0;
 
-	public function getTable($name = '', $prefix = '', $options = array())
-	{
-		$name = 'Skills';
-		$prefix = 'Table';
+        $query = $db->getQuery(true);
+        $query->insert($db->quoteName('#__claw_skills'))
+          ->columns(array_keys($record))
+          ->values(implode(',', $db->q(array_values($record))));
 
-		if ($table = $this->_createTable($name, $prefix, $options)) {
-			return $table;
-		}
+        $db->setQuery($query);
+        $db->execute();
+    
+        $success = true;
+      }
+    }
 
-		throw new \Exception(Text::sprintf('JLIB_APPLICATION_ERROR_TABLE_NAME_NOT_SUPPORTED', $name), 0);
-	}
+    if ( $success ) {
+      $app->enqueueMessage('Class copied successfully.', \Joomla\CMS\Application\CMSApplicationInterface::MSG_INFO);
+    } else {
+      $app->enqueueMessage('Class copied failed.', \Joomla\CMS\Application\CMSApplicationInterface::MSG_ERROR);
+    }
+
+    $skillRoute = Route::_('index.php?option=com_claw&view=skillssubmissions');
+    $app->redirect($skillRoute);
+
+  }
+
+  protected function loadFormData()
+  {
+    $mergeData = null;
+    $submittedFormData = Helpers::sessionGet('formdata');
+    if ($submittedFormData) {
+      $mergeData = json_decode($submittedFormData, true);
+    }
+
+    $data = $this->getItem();
+
+    if ($mergeData) {
+      foreach ($mergeData as $key => $value) {
+        $data->$key = $value;
+      }
+    }
+
+    if ($data->photo) {
+      Helpers::sessionSet('photo', $data->photo);
+    }
+
+    return $data;
+  }
+
+  public function getTable($name = '', $prefix = '', $options = array())
+  {
+    $name = 'Skills';
+    $prefix = 'Table';
+
+    if ($table = $this->_createTable($name, $prefix, $options)) {
+      return $table;
+    }
+
+    throw new \Exception(Text::sprintf('JLIB_APPLICATION_ERROR_TABLE_NAME_NOT_SUPPORTED', $name), 0);
+  }
+
+  public function GetEventInfo(): \ClawCorpLib\Lib\EventInfo
+  {
+    $events = new ClawEvents(Aliases::current);
+
+    $info = $events->getClawEventInfo();
+    return $info;
+  }
 }
