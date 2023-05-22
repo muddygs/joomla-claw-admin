5444a36e6d64705cdcb2f6cc43829c1be89a9222
muddygs <muddygs@gmail.com>
2023-05-21 23:00:57 -0400

    Restarting from scratch.

--- library/Grid/Grid.php
+++ library/Grid/Grid.php
@@ -2,15 +2,19 @@
 
 namespace ClawCorpLib\Grid;
 
+use Joomla\Database\DatabaseDriver;
+
 // This class enforces data format for received form data
 
 // TODO: This can be improved with php8
 class Grid
 {
   public int $id;
+  public int $shift_id;
+  public int $row_id;
   public string $time;
   public int $length;
-  public object $shifts;
+  public array $shifts;
 
   // TODO: Add validation
   public function __construct(array $formData)
@@ -18,21 +22,26 @@ class Grid
     $this->time = $formData['time'];
     $this->length = (int)$formData['length'];
     $this->id = $formData['id'];
+    $this->row_id = $formData['row_id'] ?? 0;
 
-    $this->shifts = (object)[];
-    foreach ( ['tue','wed','thu','fri','sat','sun','mon'] AS $day )
-    {
-      $key = $day.'pri';
-      if ( array_key_exists($key, $formData) )
-      {
-        $this->shifts->$key = (int)$formData[$key];
-      }
-      else
-      {
-        $this->shifts->$key = 0;
-      }
+    $this->shifts = [];
+    foreach (['tue', 'wed', 'thu', 'fri', 'sat', 'sun', 'mon'] as $day) {
       
+      $key = $day . 'pri';
+      if (array_key_exists($key, $formData)) {
+        $this->shifts[$key] = (int)$formData[$key];
+      } else {
+        $this->shifts[$key] = 0;
+      }
     };
+  }
 
+  public function store(DatabaseDriver $db, int $sid)
+  {
+    $this->shift_id = $sid;
+
+    if ($this->id === 0) {
+      $result = $db->insertObject('#__claw_shifts_grids', $this, 'id');
+    }
   }
 }
--- library/Grid/Grids.php
+++ library/Grid/Grids.php
@@ -1,58 +1,97 @@
 <?php
 
-namespace ClawCorpLib\Grid;
+namespace ClawCorpLib\Grids;
 
 // This class enforces data format for received form data
 
-use ClawCorp\Component\Claw\Administrator\Model\ShiftModel;
-use Joomla\CMS\Factory;
-use ClawCorpLib\Grid\Grid;
+use ClawCorpLib\Grid\GridItem;
+use ClawCorpLib\Helpers\Helpers;
+use ClawCorpLib\Lib\Aliases;
+use Joomla\Database\DatabaseDriver;
+use ClawCorpLib\Lib\ClawEvents;
 
-class Grids extends ShiftModel
+class Grids
 {
-  private Grid $grid;
-  private int $sid; // shift ID
+  /** @var ClawCorpLib/Grid/Grid[] */
+  private array $grids = [];
 
-  public function __construct(int $sid, array $formData)
-  {
-    $this->sid = $sid;
-    $this->grid = new Grid($formData);  
+  public function __construct(
+    public DatabaseDriver $db,
+    public array $coodinators = []
+  ) {
   }
 
-  public function store(): bool
+  public function createEvents()
   {
-    if ( 0 == $this->grid->id )
-    {
-      $this->grid->id = $this->nextId();
+    $events = new ClawEvents(Aliases::current);
+    $eventInfo = $events->getClawEventInfo();
+
+    $event_count = 0;
+    $this->loadGridsByEventAlias(Aliases::current);
+  }
+
+  private function loadGridsByEventAlias(string $eventAlias)
+  {
+    $query = $this->db->getQuery(true);
+    $query->select('*')
+      ->from('#__claw_shifts')
+      ->where('event = :event')
+      ->where('published = 1')
+      ->bind(':event', Aliases::current);
+    $this->db->setQuery($query);
+
+    $shifts = $this->db->loadObjectList('id');
+
+    foreach ($shifts as $s) {
+      $this->parseGridJson($s);
     }
-
-    return true;
   }
 
-  public static function loadGrids(int $sid)
+  private function parseGridJson(object $shift)
   {
+    $days = Helpers::getDays();
 
-  }
+    $grids = json_decode($shift->grid);
 
-  public static function createEvents()
-  {
-    
+    $data = get_object_vars($shift);
+
+    foreach ($grids as $g) {
+      $time = $g->time;
+      $length = $g->length;
+
+      // Loop over set days
+
+      foreach ($days as $day) {
+        $pri = $day . 'pri';
+        $event = $day . 'pri_eventid';
+        $needed = $g->$pri;
+        $event_id = $g->$event;
+
+        $grids[] = new GridItem(
+          $data['id'],
+          $time,
+          $length,
+          $data['title'],
+          $data['description'],
+          Aliases::current,
+          $data['shift_area'],
+          $data['requirements'],
+          $data['coordinators'],
+          $data['published'],
+          $day,
+          $needed,
+          $event_id
+        );
+      }
+    }
   }
 
   private function nextId(): int
   {
-    $db = Factory::getDbo();
-    $query = 'SELECT max(row_id) FROM #__claw_shifts_grids WHERE shift_id='.$db->q($this->sid);
-    $db->setQuery($query);
-    $lastId = $db->loadResult();
+    $query = 'SELECT max(row_id) FROM #__claw_shifts_grids WHERE shift_id=' . $this->db->q($this->sid);
+    $this->db->setQuery($query);
+    $lastId = $this->db->loadResult();
 
-    return !$lastId ? 1 : $lastId+1;
+    return !$lastId ? 1 : $lastId + 1;
   }
-
-  public function validate($form, $data, $group = null)
-  {
-    $this->setError("There was an error");
-    return false;
-  }
-
 }
