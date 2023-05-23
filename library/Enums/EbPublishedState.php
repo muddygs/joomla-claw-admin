8058b4e51eca322295b6f73b71ade334130ef4f2
muddygs <muddygs@gmail.com>
2023-05-22 22:42:14 -0400

    Cleanup only.

--- library/Enums/EbPublishedState.php
+++ library/Enums/EbPublishedState.php
@@ -1,10 +1,11 @@
 <?php
+
 namespace ClawCorpLib\Enums;
 
-enum EbPublishedState : int
+enum EbPublishedState: int
 {
-    case any = 0;
-    case published = 1;
-    case cancelled = 2;
-    case waitlist = 3;
-  }
+  case any = 0;
+  case published = 1;
+  case cancelled = 2;
+  case waitlist = 3;
+}
