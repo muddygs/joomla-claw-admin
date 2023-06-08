75dc0ae6356d56297a77843ee563378516c57d08
muddygs <muddygs@gmail.com>
2023-06-07 21:13:07 -0400

    A bit of ACL.

--- component/admin/src/View/Sponsors/HtmlView.php
+++ component/admin/src/View/Sponsors/HtmlView.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @package     ClawCorp
  * @subpackage  com_claw
@@ -30,132 +31,120 @@ use Joomla\CMS\Toolbar\ToolbarHelper;
  */
 class HtmlView extends BaseHtmlView
 {
-	/**
-	 * The search tools form
-	 *
-	 * @var    Form
-	 * @since  1.6
-	 */
-	public $filterForm;
+  /**
+   * The search tools form
+   *
+   * @var    Form
+   * @since  1.6
+   */
+  public $filterForm;
 
-	/**
-	 * The active search filters
-	 *
-	 * @var    array
-	 * @since  1.6
-	 */
-	public $activeFilters = [];
+  /**
+   * The active search filters
+   *
+   * @var    array
+   * @since  1.6
+   */
+  public $activeFilters = [];
 
-	/**
-	 * Category data
-	 *
-	 * @var    array
-	 * @since  1.6
-	 */
-	protected $categories = [];
+  /**
+   * Category data
+   *
+   * @var    array
+   * @since  1.6
+   */
+  protected $categories = [];
 
-	/**
-	 * An array of items
-	 *
-	 * @var    array
-	 * @since  1.6
-	 */
-	protected $items = [];
+  /**
+   * An array of items
+   *
+   * @var    array
+   * @since  1.6
+   */
+  protected $items = [];
 
-	/**
-	 * The pagination object
-	 *
-	 * @var    Pagination
-	 * @since  1.6
-	 */
-	protected $pagination;
+  /**
+   * The pagination object
+   *
+   * @var    Pagination
+   * @since  1.6
+   */
+  protected $pagination;
 
-	/**
-	 * The model state
-	 *
-	 * @var    Registry
-	 * @since  1.6
-	 */
-	protected $state;
+  /**
+   * The model state
+   *
+   * @var    Registry
+   * @since  1.6
+   */
+  protected $state;
 
-	/**
-	 * @param   string  $tpl  The name of the template file to parse; automatically searches through the template paths.
-	 * @return  void
-	 */
-	function display($tpl = null)
-	{
-		$this->state      = $this->get('State');
-		$this->items      = $this->get('Items');
-		$this->pagination = $this->get('Pagination');
-		$this->filterForm    = $this->get('FilterForm');
-		$this->activeFilters = $this->get('ActiveFilters');
+  /**
+   * @param   string  $tpl  The name of the template file to parse; automatically searches through the template paths.
+   * @return  void
+   */
+  function display($tpl = null)
+  {
+    $this->state      = $this->get('State');
+    $this->items      = $this->get('Items');
+    $this->pagination = $this->get('Pagination');
+    $this->filterForm    = $this->get('FilterForm');
+    $this->activeFilters = $this->get('ActiveFilters');
 
-		// Check for errors.
-		if (count($errors = $this->get('Errors'))) {
-			throw new GenericDataException(implode("\n", $errors), 500);
-		}
+    // Check for errors.
+    if (count($errors = $this->get('Errors'))) {
+      throw new GenericDataException(implode("\n", $errors), 500);
+    }
 
-		$this->addToolbar();
+    $this->addToolbar();
 
-		parent::display($tpl);
-	}
+    parent::display($tpl);
+  }
 
-	protected function addToolbar(): void
-	{
-		$app = Factory::getApplication();
+  protected function addToolbar(): void
+  {
+    $app = Factory::getApplication();
 
-		ToolbarHelper::title('CLAW Sponsors');
+    ToolbarHelper::title('CLAW Sponsors');
 
-		// Get the toolbar object instance
-		$toolbar = Toolbar::getInstance('toolbar');
+    // Get the toolbar object instance
+    $toolbar = Toolbar::getInstance('toolbar');
 
-		//$user  = $app->getIdentity();
+    $user  = $app->getIdentity();
 
-		// if ($user->authorise('core.admin', 'com_countrybase'))
-		// {
-		$toolbar->addNew('sponsor.add');
-		// }
+    if ($user->authorise('core.admin', 'com_claw')) {
+      $toolbar->addNew('sponsor.add');
+    }
 
-		// if ($user->authorise('core.edit.state', 'com_countrybase'))
-		// {
-		// 	$dropdown = $toolbar->dropdownButton('status-group')
-		// 	->text('JTOOLBAR_CHANGE_STATUS')
-		// 	->toggleSplit(false)
-		// 	->icon('icon-ellipsis-h')
-		// 	->buttonClass('btn btn-action')
-		// 	->listCheck(true);
+    // if ($user->authorise('core.edit.state', 'com_countrybase'))
+    // {
+    // 	$dropdown = $toolbar->dropdownButton('status-group')
+    // 	->text('JTOOLBAR_CHANGE_STATUS')
+    // 	->toggleSplit(false)
+    // 	->icon('icon-ellipsis-h')
+    // 	->buttonClass('btn btn-action')
+    // 	->listCheck(true);
 
-		// 	$childBar = $dropdown->getChildToolbar();
+    // 	$childBar = $dropdown->getChildToolbar();
 
-		// 	$childBar->publish('countries.publish')->listCheck(true);
+    // 	$childBar->publish('countries.publish')->listCheck(true);
 
-		// 	$childBar->unpublish('countries.unpublish')->listCheck(true);
+    // 	$childBar->unpublish('countries.unpublish')->listCheck(true);
 
-		// 	$childBar->archive('countries.archive')->listCheck(true);
+    // 	$childBar->archive('countries.archive')->listCheck(true);
 
-		// 	if ($this->state->get('filter.published') != -2)
-		// 	{
-		// 		$childBar->trash('countries.trash')->listCheck(true);
-		// 	}
-		// }
+    // 	if ($this->state->get('filter.published') != -2)
+    // 	{
+    // 		$childBar->trash('countries.trash')->listCheck(true);
+    // 	}
+    // }
 
-		// if ($this->state->get('filter.published') == -2 && $user->authorise('core.delete', 'com_countrybase'))
-		// {
-		// 	$toolbar->delete('countries.delete')
-		// 	->text('JTOOLBAR_EMPTY_TRASH')
-		// 	->message('JGLOBAL_CONFIRM_DELETE')
-		// 	->listCheck(true);
-		// }
-
-		// if ($user->authorise('core.admin', 'com_countrybase') || $user->authorise('core.options', 'com_countrybase'))
-		// {
-		// 	$toolbar->preferences('com_countrybase');
-		// }
-
-		// $tmpl = $app->input->getCmd('tmpl');
-		// if ($tmpl !== 'component')
-		// {
-		// 	ToolbarHelper::help('countrybase', true);
-		// }
-	}
+    // if ($this->state->get('filter.published') == -2 && $user->authorise('core.delete', 'com_countrybase'))
+    // {
+    // 	$toolbar->delete('countries.delete')
+    // 	->text('JTOOLBAR_EMPTY_TRASH')
+    // 	->message('JGLOBAL_CONFIRM_DELETE')
+    // 	->listCheck(true);
+    // }
+  }
 }
