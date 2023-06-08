480da071cd4c27d073d7c362fdabbcae687edbcb
muddygs <muddygs@gmail.com>
2023-06-07 21:16:51 -0400

    Dynamically determine database object Added location by id

--- library/Helpers/Locations.php
+++ library/Helpers/Locations.php
@@ -1,13 +1,16 @@
 <?php
 namespace ClawCorpLib\Helpers;
 
+use Joomla\CMS\Factory;
 use Joomla\Database\DatabaseDriver;
 
 class Locations {
   private static array $cache = [];
 
-  public static function GetLocationsList(DatabaseDriver $db): array {
+  public static function GetLocationsList(): array {
     if ( count(Locations::$cache) > 0) return Locations::$cache;
+
+    $db = Factory::getContainer()->get('DatabaseDriver');
 
     $query = $db->getQuery(true);
 
@@ -18,5 +21,11 @@ class Locations {
     $db->setQuery($query);
     Locations::$cache = $db->loadObjectList('id') ?? [];
     return Locations::$cache;
+  }
+
+  public static function GetLocationById(int $id): ?object
+  {
+    if ( !count(Locations::$cache) ) Locations::GetLocationsList();
+    return Locations::$cache[$id] ?? (object)['value' => ''];
   }
 }
