5f909a419bb6ed352fb98db3f013eb4ca57119cc
muddygs <muddygs@gmail.com>
2023-06-15 21:08:07 -0400

    Probably not needed; main controller for this namespace should already handle this.

---
+++ component/site/src/Controller/RegistrationsurveyController.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @package     ClawCorp
+ * @subpackage  com_claw
+ *
+ * @copyright   (C) 2022 C.L.A.W. Corp. All Rights Reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+
+namespace ClawCorp\Component\Claw\Site\Controller;
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\MVC\Controller\BaseController;
+
+/**
+ * Controller for report views
+ *
+ * @since  1.6
+ */
+class RegistrationsurveyController extends BaseController
+{
+  // TODO: Delete this file?
+}
