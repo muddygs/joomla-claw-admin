5ce9a6272fe7286657a19b47039ddbf83a1c5b87
muddygs <muddygs@gmail.com>
2023-06-24 07:17:45 -0400

    Primitive dashboard. Currently only links to coupon generator.

--- component/admin/tmpl/claw/default.php
+++ component/admin/tmpl/claw/default.php
@@ -10,9 +10,54 @@
  // No direct access to this file
 defined('_JEXEC') or die('Restricted Access');
 
-use Joomla\CMS\Language\Text;
+use ClawCorpLib\Helpers\Bootstrap;
+
+$tags = [
+  ['<h4 class="fw-bold mb-0">','</h4>'],
+  ['<p>','</p>']
+];
+
 
 ?>
-<h2><?php echo Text::_('COM_CLAW_DASHBOARD_PARAMS') ?></h2>
+<h1>CLAW Dashboard</h1>
 
-<a href="/administrator/index.php?option=com_claw&view=coupongenerator&layout=edit">Coupon Generator</a>
+<h2>Event Management</h2>
+<?php
+$content = [
+    'ticket-alt' => ['Event Schedule','LINK'],
+    'splotch' => ['Sponsors','LINK'],
+    'shopping-basket' => ['Vendors','LINK'],
+    'map-signs' => ['Locations','LINK'],
+  ];
+  
+Bootstrap::writeGrid($content, $tags);
+?>
+<h2>Volunteer Management</h2>
+<?php
+$content = [
+  'people-carry' => ['Shifts','LINK'],
+];
+
+Bootstrap::writeGrid($content, $tags);
+
+?>
+<h2>Skills &amp; Eductions</h2>
+<?php
+$content = [
+    'ticket-alt' => ['Presenter Submissions','LINK'],
+    'user-tag' => ['Class Submissions','LINK'],
+  ];
+  
+Bootstrap::writeGrid($content, $tags);
+?>
+
+
+<h2>Administration Tools</h2>
+<?php
+$content = [
+    'ticket-alt' => ['Coupon Generator','<a href="/administrator/index.php?option=com_claw&view=coupongenerator&layout=edit" role="button" class="btn btn-danger">Launch</a>'],
+    #'user-tag' => ['CLAW Nation','Please enter your CLAW Nation coupon code above and click Start Registration or contact <a href="/planning/guest-services?category_id=10">guest services</a> to obtain your registration coupon - CLAW NATION ONLY.'],
+  ];
+
+Bootstrap::writeGrid($content, $tags);
+
