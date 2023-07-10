924ab6d3b087ff965d5db3347f82d7a14b31e306
muddygs <muddygs@gmail.com>
2023-07-09 21:26:09 -0400

    Cleanup old comments and use statements

--- component/admin/src/View/Skills/HtmlView.php
+++ component/admin/src/View/Skills/HtmlView.php
@@ -17,10 +17,6 @@ use Joomla\CMS\Factory;
 use Joomla\CMS\Toolbar\Toolbar;
 use Joomla\CMS\Toolbar\ToolbarHelper;
 
-use ClawCorpLib\Helpers\Helpers;
-use ClawCorpLib\Lib\Aliases;
-use Symfony\Component\DependencyInjection\Alias;
-
 /**
  * View class for CLAW Skills & Education listing
  *
@@ -120,47 +116,5 @@ class HtmlView extends BaseHtmlView
 			->text('Delete')
 			->listCheck(true);
 		}
-
-		// if ($user->authorise('core.edit.state', 'com_countrybase'))
-		// {
-		// 	$dropdown = $toolbar->dropdownButton('status-group')
-		// 	->text('JTOOLBAR_CHANGE_STATUS')
-		// 	->toggleSplit(false)
-		// 	->icon('icon-ellipsis-h')
-		// 	->buttonClass('btn btn-action')
-		// 	->listCheck(true);
-
-		// 	$childBar = $dropdown->getChildToolbar();
-
-		// 	$childBar->publish('countries.publish')->listCheck(true);
-
-		// 	$childBar->unpublish('countries.unpublish')->listCheck(true);
-
-		// 	$childBar->archive('countries.archive')->listCheck(true);
-
-		// 	if ($this->state->get('filter.published') != -2)
-		// 	{
-		// 		$childBar->trash('countries.trash')->listCheck(true);
-		// 	}
-		// }
-
-		// if ($this->state->get('filter.published') == -2 && $user->authorise('core.delete', 'com_countrybase'))
-		// {
-		// 	$toolbar->delete('countries.delete')
-		// 	->text('JTOOLBAR_EMPTY_TRASH')
-		// 	->message('JGLOBAL_CONFIRM_DELETE')
-		// 	->listCheck(true);
-		// }
-
-		// if ($user->authorise('core.admin', 'com_countrybase') || $user->authorise('core.options', 'com_countrybase'))
-		// {
-		// 	$toolbar->preferences('com_countrybase');
-		// }
-
-		// $tmpl = $app->input->getCmd('tmpl');
-		// if ($tmpl !== 'component')
-		// {
-		// 	ToolbarHelper::help('countrybase', true);
-		// }
 	}
 }
