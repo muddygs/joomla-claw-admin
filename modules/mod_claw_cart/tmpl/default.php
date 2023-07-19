3fabb4c1339fb6f35e1e4cd3447d0faaa1d1c601
muddygs <muddygs@gmail.com>
2023-07-18 22:00:38 -0400

    Tiny module to display cart icon when user signed in (use module view level for when to display)

---
+++ modules/mod_claw_cart/tmpl/default.php
@@ -0,0 +1,9 @@
+<?php
+defined('_JEXEC') or die;
+
+?>
+<div name="clawcart">
+	<a href="/index.php?option=com_eventbooking&view=cart" class="claw-cart" aria-label="CLAW Cart">
+		<span class="fa fa-shopping-cart" aria-hidden="true"></span>
+	</a>
+</div>
