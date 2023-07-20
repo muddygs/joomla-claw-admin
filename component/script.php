af8bf12eb81459957a7edfcfe66e187019077fe8
muddygs <muddygs@gmail.com>
2023-07-20 07:56:02 -0400

    Implimented InstallerAdapter from J4 documentation; now setup to seed database if necessary

--- component/script.php
+++ component/script.php
@@ -1,27 +1,116 @@
 <?php
-/**
- * @package     ClawCorp
- * @subpackage  com_claw
- *
- * @copyright   (C) 2022 C.L.A.W. Corp. All Rights Reserved.
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
- */
 
-defined('_JEXEC') or die('Restricted access');
-
-use Joomla\CMS\Component\ComponentHelper;
+use Joomla\CMS\Installer\InstallerAdapter;
 use Joomla\CMS\Factory;
+use Joomla\Database\DatabaseDriver;
 
 class com_clawInstallerScript
 {
-
 	/**
-	 * method to run after an install/update/uninstall method
+	 * Constructor
 	 *
-	 * @return void
+	 * @param   InstallerAdapter  $adapter  The object responsible for running this script
 	 */
-	public function postflight($type, $parent)
+	public function __construct(InstallerAdapter $adapter)
+	{
+	}
+	
+	/**
+	 * Called before any type of action
+	 *
+	 * @param   string  $route  Which action is happening (install|uninstall|discover_install|update)
+	 * @param   InstallerAdapter  $adapter  The object responsible for running this script
+	 *
+	 * @return  boolean  True on success
+	 */
+	public function preflight($route, InstallerAdapter $adapter)
 	{
 		return true;
+	}
+	
+	/**
+	 * Called after any type of action
+	 *
+	 * @param   string  $route  Which action is happening (install|uninstall|discover_install|update)
+	 * @param   InstallerAdapter  $adapter  The object responsible for running this script
+	 *
+	 * @return  boolean  True on success
+	 */
+	public function postflight($route, $adapter)
+	{
+		if ( !in_array($route, ['install', 'update', 'discover_install']) ) {
+			return true;
+		}
+
+		// Verify that base key/value pairs exist in #__claw_field_values
+		$db = Factory::getContainer()->get('DatabaseDriver');
+		$query = $db->getQuery(true);
+		$query->select('COUNT(*)')
+			->from($db->qn('#__claw_field_values'));
+		$db->setQuery($query);
+
+		if (!$db->loadResult())
+		{
+			$sqlFile = JPATH_ADMINISTRATOR . '/components/com_claw/sql/config.fieldvalues.sql';
+			$this->executeSqlFile($db, $sqlFile);
+		}
+	
+		return true;
+	}
+	
+	/**
+	 * Called on installation
+	 *
+	 * @param   InstallerAdapter  $adapter  The object responsible for running this script
+	 *
+	 * @return  boolean  True on success
+	 */
+	public function install(InstallerAdapter $adapter)
+	{
+		return true;
+	}
+	
+	/**
+	 * Called on update
+	 *
+	 * @param   InstallerAdapter  $adapter  The object responsible for running this script
+	 *
+	 * @return  boolean  True on success
+	 */
+	public function update(InstallerAdapter $adapter)
+	{
+		return true;
+	}
+	
+	/**
+	 * Called on uninstallation
+	 *
+	 * @param   InstallerAdapter  $adapter  The object responsible for running this script
+	 */
+	public function uninstall(InstallerAdapter $adapter)
+	{
+		return true;
+	}
+
+	/**
+	 * Execute queries from the given file
+	 *
+	 * @param   string  $file
+	 */
+	public function executeSqlFile(DatabaseDriver $db, string $file)
+	{
+		$sql     = file_get_contents($file);
+		$queries = $db->splitSql($sql);
+
+		foreach ($queries as $query)
+		{
+			$query = trim($query);
+
+			if ($query != '' && $query[0] != '#')
+			{
+				$db->setQuery($query)
+					->execute();
+			}
+		}
 	}
 }
