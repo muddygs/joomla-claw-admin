7d761b3750abc4199e877e7e4cd9ce9a7d4398e9
muddygs <muddygs@gmail.com>
2023-09-04 08:31:46 -0400

    Moved my working path

--- rsync.sh
+++ rsync.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#rsync -arv --delete /var/www/eb/public_html/administrator/components/com_claw/ component/admin/
-rsync -arv /var/www/eb/public_html/administrator/components/com_claw/ component/admin/
-rsync -arv /var/www/eb/public_html/components/com_claw/ component/site/
+#rsync -arv --delete /var/www/j4_dev/public_html/administrator/components/com_claw/ component/admin/
+rsync -arv /var/www/j4_dev/public_html/administrator/components/com_claw/ component/admin/
+rsync -arv /var/www/j4_dev/public_html/components/com_claw/ component/site/
 mv component/admin/claw.xml component/
 mv component/admin/script.php component/
-#rsync -arv --delete /var/www/eb/public_html/libraries/claw/ library/
-rsync -arv /var/www/eb/public_html/libraries/claw/ library/
-#rsync -arv /var/www/eb/public_html/plugins/system/claw/ plugin/
-rsync -arv /var/www/eb/public_html/media/com_claw/ media_raw/
-rsync -arv /var/www/eb/public_html/modules/mod_claw_sponsors modules/
-rsync -arv /var/www/eb/public_html/modules/mod_claw_cart modules/
-rsync -arv /var/www/eb/public_html/modules/mod_claw_vendors modules/
-rsync -arv /var/www/eb/public_html/modules/mod_claw_regbuttons modules/
+#rsync -arv --delete /var/www/j4_dev/public_html/libraries/claw/ library/
+rsync -arv /var/www/j4_dev/public_html/libraries/claw/ library/
+#rsync -arv /var/www/j4_dev/public_html/plugins/system/claw/ plugin/
+rsync -arv /var/www/j4_dev/public_html/media/com_claw/ media_raw/
+rsync -arv /var/www/j4_dev/public_html/modules/mod_claw_sponsors modules/
+rsync -arv /var/www/j4_dev/public_html/modules/mod_claw_cart modules/
+rsync -arv /var/www/j4_dev/public_html/modules/mod_claw_vendors modules/
+rsync -arv /var/www/j4_dev/public_html/modules/mod_claw_regbuttons modules/
