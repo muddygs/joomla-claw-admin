ea8b306d8d90c7f9b869e579c593070e46ef587a
muddygs <muddygs@gmail.com>
2023-10-09 18:35:25 -0400

    Adds "claw-no-admin" to class to auto hide within checkin interfaces

--- modules/mod_claw_cart/tmpl/default.php
+++ modules/mod_claw_cart/tmpl/default.php
@@ -2,7 +2,7 @@
 defined('_JEXEC') or die;
 
 ?>
-<div name="clawcart">
+<div name="clawcart" class="claw-no-admin" id="clawcart">
 	<a href="/index.php?option=com_eventbooking&view=cart" class="claw-cart" aria-label="CLAW Cart">
 		<span class="fa fa-shopping-cart" aria-hidden="true"></span>
 	</a>
