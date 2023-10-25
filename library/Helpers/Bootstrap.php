d49d0ad49e0711ea72818fe14aff68bebadba9fd
muddygs <muddygs@gmail.com>
2023-10-25 17:00:57 -0400

    Temporarily using CDN for bootstrap Reordered load according to bootstrap documentation

--- library/Helpers/Bootstrap.php
+++ library/Helpers/Bootstrap.php
@@ -139,18 +139,16 @@ class Bootstrap
       <meta http-equiv="Pragma" content="no-cache" />
       <meta http-equiv="Expires" content="0" />
       <title>READY TO PRINT</title>
-      <script src="/templates/shaper_helixultimate/js/bootstrap.bundle.min.js"></script>
-      <script src="/templates/shaper_helixultimate/js/lazysizes.min.js"></script>
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <?php
       foreach ( $js AS $j ) {
         echo "<script src=\"$j?$ts\"></script>";
       }
     ?>
-      <link href="/templates/shaper_helixultimate/css/bootstrap.min.css" rel="stylesheet" />
       <link href="/templates/shaper_helixultimate/css/font-awesome.min.css" rel="stylesheet" />
       <?php
       foreach ( $css AS $c ) {
-        echo "<link href=\"$c?$ts\" rel=\"stylesheet\" />";
+        echo "<link href=\"$c?$ts\" rel=\"stylesheet\" />\n";
       }
     ?>
     </head>
@@ -159,11 +157,12 @@ class Bootstrap
   <?php
   }
 
-  public static function rawFooter(): string
+  public static function rawFooter()
   {
-    return <<< HTML
+    ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     </body>
     </html>
-HTML;
+    <?php
   }
 }
