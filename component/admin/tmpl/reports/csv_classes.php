850d23127c4fa34bf0c93e1664b704ce8f807c67
muddygs <muddygs@gmail.com>
2023-11-03 07:07:45 -0400

    Adding csv export templates for classes and presenters

---
+++ component/admin/tmpl/reports/csv_classes.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @package     ClawCorp
+ * @subpackage  com_claw
+ *
+ * @copyright   (C) 2023 C.L.A.W. Corp. All Rights Reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+ // No direct access to this file
+\defined('_JEXEC') or die('Restricted Access');
+
+use ClawCorpLib\Helpers\Skills;
+use ClawCorpLib\Lib\Aliases;
+use Joomla\CMS\Factory;
+use Joomla\CMS\HTML\HTMLHelper;
+
+$skills = new Skills(Factory::getContainer()->get('DatabaseDriver'), Aliases::current());
+$filename = 'Classes_Export_' . HtmlHelper::date('now', 'Y-m-d_H-i-s') . '.csv';
+$skills->classesCSV($filename);
