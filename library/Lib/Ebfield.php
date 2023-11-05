7468a70d238d9956e4a55074e83bae06a1202815
muddygs <muddygs@gmail.com>
2023-11-05 12:43:37 -0500

    Wrapper for processing an Event Booking eb_fields table

---
+++ library/Lib/Ebfield.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace ClawCorpLib\Lib;
+
+use InvalidArgumentException;
+use Joomla\CMS\Factory;
+
+\defined('_JEXEC') or die;
+
+class Ebfield
+{
+  public int $id = 0;
+  private object $field;
+
+  public function __construct(
+    public string $name)
+  {
+    $this->field = (object)[];
+    $this->loadField();
+
+    if ( 0 == $this->id ) {
+      throw new InvalidArgumentException('Field name not found: ' . $this->name);
+    }
+
+  }
+
+  private function loadField()
+  {
+    $db = Factory::getContainer()->get('DatabaseDriver');
+    $query = $db->getQuery(true);
+    $query
+        ->select('*')
+        ->from('#__eb_fields')
+        ->where($db->quoteName('name') . ' = ' . $db->quote($this->name));
+    $db->setQuery($query);
+    $field = $db->loadObject();
+
+    if ( $field ) {
+      $this->id = $field->id;
+      $this->field = $field;
+    }
+  }
+
+  public function get(string $property) {
+    if ( property_exists($this->field, $property) ) {
+      return $this->field->$property;
+    }
+
+    throw new InvalidArgumentException('Field property not found: ' . $property);
+  }
+
+  /**
+   * Returns count of field values used for a given event. Key: field_value, Value: value_count
+   * @param int $eventId 
+   * @return null|array 
+   * @throws InvalidActionException 
+   * @throws KeyNotFoundException 
+   */
+  public function valueCounts(int $eventId): ?array
+  {
+    if ( !in_array($this->field->fieldtype, ['List', 'Checkboxes']) ) {
+      throw new InvalidArgumentException('Field type not supported: ' . $this->field->fieldtype);
+    }
+
+    $db = Factory::getContainer()->get('DatabaseDriver');
+    $query = $db->getQuery(true);
+    $query
+        ->select('fv.field_value, COUNT(*) as value_count')
+        ->from('#__eb_field_values AS fv')
+        ->join('INNER', '#__eb_registrants AS r ON fv.registrant_id = r.id AND r.event_id=' . $eventId)
+        ->where('r.published = 1')
+        ->where('fv.field_id = ' . $this->id)
+        ->group('fv.field_value');
+    $db->setQuery($query);
+    $fieldValues = $db->loadObjectList();
+
+    return $fieldValues;
+  }
+}
