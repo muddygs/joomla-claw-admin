c35615e466a0ad3fb37fb2a8444fed22f580df4b
muddygs <muddygs@gmail.com>
2023-11-09 13:24:36 -0500

    Fixed day/time ordering* and individual event title display * as long as event doesn't cross a year boundary

--- component/admin/tmpl/reports/volunteer_detail.php
+++ component/admin/tmpl/reports/volunteer_detail.php
@@ -15,35 +15,40 @@ use ClawCorpLib\Helpers\Helpers;
 
 \ClawCorpLib\Helpers\Bootstrap::rawHeader([], ['/media/com_claw/css/print_letter.css']);
 
+$days = Helpers::getDays();
+
 ?>
 <h1 class="text-center">Volunteer Detail Report</h1>
 <?php
-foreach ($this->items['shifts'] as $this->sid => $events) {
+foreach ($this->items['shifts'] as $this->sid => $shift) {
   $grid = [];
 
   // Determine ordering by event_date
-  foreach ($events as $e) {
+  foreach ($shift as $s) {
     // Time only
-    $stime = date('H:i', strtotime($e->event_date));
-    $etime = date('H:i', strtotime($e->event_end_date));
+    $stime = date('H:i', strtotime($s->event_date));
+    $etime = date('H:i', strtotime($s->event_end_date));
+    $dow = str_pad(date('z', strtotime($s->event_date)), 3, '0', STR_PAD_LEFT);
 
     // Will sort on this key
-    $k = $stime . '-' . $etime;
+    $k = implode(':', [$dow, $stime, $etime]);
 
     if (!array_key_exists($k, $grid)) {
       $grid[$k] = (object)[];
-      foreach (Helpers::getDays() as $day) {
+
+      foreach ($days as $day) {
         $grid[$k]->$day = null;
       }
     }
 
-    $day = strtolower(date('D', strtotime($e->event_date)));
+    $day = strtolower(date('D', strtotime($s->event_date)));
 
     $grid[$k]->$day = (object) [
-      'event_capacity' => $e->event_capacity,
-      'memberCount' => $e->memberCount,
-      'published' => $e->published,
-      'id' => $e->id
+      'event_capacity' => $s->event_capacity,
+      'memberCount' => $s->memberCount,
+      'published' => $s->published,
+      'id' => $s->id,
+      'title' => $s->title,
     ];
   }
 
@@ -51,17 +56,18 @@ foreach ($this->items['shifts'] as $this->sid => $events) {
 
   if (!array_key_exists($this->sid, $this->items['coordinators'])) {
     echo "<h1 class=\"text-danger\">Cannot find coordinator for: $this->sid</h1>";
-    var_dump($e);
+    var_dump($s);
     continue;
   }
 ?>
   <?php
-
   foreach ($grid as $k => $row) {
-    foreach ( $row as $this->shift_info ) {
-      if (is_null($this->shift_info)) continue;
+    foreach ( $row AS $day => $this->shift_info ) {
+      // Skip empty shifts and other meta data
+      if (is_null($this->shift_info) || !in_array($day, $days)) continue;
+
       ?>
-        <h1><?= $e->title ?></h1>
+        <h1><?= $this->shift_info->title ?></h1>
         <h2><?= $this->items['coordinators'][$this->sid]['name'] ?> (<?= $this->items['coordinators'][$this->sid]['email'] ?>)</h2>
       <?php
       echo $this->loadTemplate('registrants');
