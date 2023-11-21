3b99e5e395504286ad10cc9fb7348279b2b98b37
muddygs <muddygs@gmail.com>
2023-11-20 19:26:09 -0500

    Wrapper class for Joomla redirects component management

---
+++ library/Helpers/Redirects.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace ClawCorpLib\Helpers;
+
+use Joomla\CMS\Factory;
+use Joomla\Database\DatabaseDriver;
+
+class Redirects
+{
+  private array $redirectLinksColumns;
+  private object $redirect;
+
+  public function __construct(
+    public DatabaseDriver $db,
+    public string $oldUrl,
+    public string $newUrl,
+    public string $comment = '', // Used as an id for tracking future updates, blank to ignore
+    public string $header = '301',
+  ) {
+    $this->redirectLinksColumns = array_keys($this->db->getTableColumns('#__redirect_links'));
+    $this->setDefaults();
+  }
+
+  private function setDefaults()
+  {
+    // Set date
+    $date = Factory::getDate()->toSql();
+
+    $this->redirect =
+      (object)[
+        'id' => 0,
+        'old_url' => $this->oldUrl,
+        'new_url' => $this->newUrl,
+        'referer' => '',
+        'comment' => $this->comment,
+        'hits' => '0',
+        'published' => 1,
+        'created_date' => $date,
+        'modified_date' => $date,
+        'header' => $this->header
+      ];
+  }
+
+  public function get(): object
+  {
+    return $this->redirect;
+  }
+
+  public function set(string $key, string $value)
+  {
+    if (in_array($key, $this->redirectLinksColumns)) {
+      $this->redirect->$key = $value;
+    } else {
+      throw new \Exception("Invalid redirect key: " . $key);
+    }
+  }
+
+  public function insert(): int
+  {
+    // If comment is not blank, try to delete equivalent redirect first
+    if ($this->comment) {
+      $query = $this->db->getQuery(true);
+      $conditions = [
+        $query->qn('comment') . ' = ' . $query->q($this->comment)
+      ];
+
+      $query->delete($this->db->qn('#__redirect_links'))
+        ->where($conditions);
+      
+      $this->db->setQuery($query);
+      $this->db->execute();
+    }
+
+    $result = $this->db->insertObject('#__redirect_links', $this->redirect, 'id');
+
+    return $result != false ? $this->redirect->id : 0;
+  }
+}
