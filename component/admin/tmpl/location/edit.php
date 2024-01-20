34829c1094e2f75098f1db29c2f5d1b8c9fd901e
muddygs <muddygs@gmail.com>
2024-01-19 22:53:40 -0500

    Removed unnecessary tab to simplify form display.

--- component/admin/tmpl/location/edit.php
+++ component/admin/tmpl/location/edit.php
@@ -3,7 +3,7 @@
  * @package     ClawCorp
  * @subpackage  com_claw
  *
- * @copyright   (C) 2022 C.L.A.W. Corp. All Rights Reserved.
+ * @copyright   (C) 2024 C.L.A.W. Corp. All Rights Reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
@@ -17,7 +17,6 @@ HTMLHelper::_('behavior.keepalive');
 
 // Set default (should one not exist, model will assign
 $ordering = $this->item->ordering ?? -1;
-$currentParent = $this->item->catid;
 
 $view = 'location';
 
@@ -30,18 +29,12 @@ $view = 'location';
 	method="post" name="adminForm" id="<?php echo $view ?>-form" class="form-validate">
 
 	<div>
-		<?php echo HTMLHelper::_('uitab.startTabSet', 'myTab', array('active' => 'details')); ?>
-
-		<?php echo HTMLHelper::_('uitab.addTab', 'myTab', 'details', 'Details'); ?>
 		<div class="row">
 			<?php echo $this->form->renderField('catid'); ?>
 			<?php echo $this->form->renderField('value'); ?>
 			<?php echo $this->form->renderField('alias'); ?>
 			<?php echo $this->form->renderField('id'); ?>
 		</div>
-		<?php echo HTMLHelper::_('uitab.endTab'); ?>
-
-		<?php echo HTMLHelper::_('uitab.endTabSet'); ?>
 	</div>
 	
 	<input type="hidden" name="ordering" value="<?php echo $ordering ?>"/>
