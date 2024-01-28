47124d52f4b89700fedc0ed6cab90bb569a28f66
muddygs <muddygs@gmail.com>
2024-01-28 12:31:24 -0500

    Temporary fix to backport to MariaDB 10.5 since it lacks json_keys.

--- component/admin/src/Model/SpeeddatinginfosModel.php
+++ component/admin/src/Model/SpeeddatinginfosModel.php
@@ -32,13 +32,26 @@ class SpeeddatinginfosModel extends EventconfigsModel
    */
   protected function getListQuery()
   {
+    // MariaDB 10.5.8 does not support the JSON_TABLE function.
+/*    $query = <<<SQL
+    SELECT JSON_KEYS(a.meta) as metaKeys
+FROM #__claw_packages AS a
+WHERE a.eventAlias = 'c0424 'AND a.packageInfoType IN(4);
+SQL;
+
+    $this->db->setQuery($query);
+    $metaKeys = $this->db->loadColumn();
+*/
+
     $query = parent::getListQuery();
 
     $selects = [
       'a.*',
-      'jt.key_name',
-      'JSON_UNQUOTE(JSON_EXTRACT(a.meta, CONCAT(\'$.\', jt.key_name, \'.role\'))) AS role',
-      'JSON_UNQUOTE(JSON_EXTRACT(a.meta, CONCAT(\'$.\', jt.key_name, \'.eventId\'))) AS eventId',
+      //'jt.key_name',
+//  10.6:    'JSON_UNQUOTE(JSON_EXTRACT(a.meta, CONCAT(\'$.\', jt.key_name, \'.role\'))) AS role',
+  // 10.6:   'JSON_UNQUOTE(JSON_EXTRACT(a.meta, CONCAT(\'$.\', jt.key_name, \'.eventId\'))) AS eventId',
+      'JSON_UNQUOTE(JSON_EXTRACT(a.meta, CONCAT(\'$.meta0.role\'))) AS role',
+      'JSON_UNQUOTE(JSON_EXTRACT(a.meta, CONCAT(\'$.meta0.eventId\'))) AS eventId',
     ];
 
     // Since the base select cannot be appended, we just redeclare it here with everything
@@ -54,7 +67,7 @@ class SpeeddatinginfosModel extends EventconfigsModel
     $query->where('a.packageInfoType IN (' . implode(',', $packageInfoTypes) . ')');
     // Join over the JSON table.
     // $query->join('INNER', 'JSON_TABLE(a.meta, \'$[*]\' COLUMNS (subgroup VARCHAR(50) PATH \'$\')) AS j');
-    $query->join('CROSS', 'JSON_TABLE(JSON_KEYS(a.meta), \'$[*]\' COLUMNS (key_name VARCHAR(255) PATH \'$\')) AS jt');
+    // TODO: Fix for MariaDB 10.5: $query->join('CROSS', 'JSON_TABLE(JSON_KEYS(a.meta), \'$[*]\' COLUMNS (key_name VARCHAR(255) PATH \'$\')) AS jt');
 
     // Initialize the row number variable.
     // $this->db->setQuery('SET @row_number = 0;');
