73851d5fe34feefa0f47764ee1291d51e344dd19
muddygs <muddygs@gmail.com>
2024-03-18 10:25:15 -0400

    Simplified kiosk view to just sponsor name/type (no graphics)

--- modules/mod_claw_schedule/tmpl/upcoming.php
+++ modules/mod_claw_schedule/tmpl/upcoming.php
@@ -31,38 +31,57 @@ if ($currentHour >= 5 && $currentHour < 12) {
     </tr>
   </thead>
   <tbody>
-    <?php foreach ($events as $event) : ?>
+    <?php foreach ($events as $event) : 
+      $order = 0;
+    ?>
       <tr>
         <td><?= $event->start_time; ?></td>
         <td><?= $event->event_title; ?></td>
         <td><?= Locations::GetLocationById($event->location)->value ?></td>
         <td>
           <?php
-            $sponsors = json_decode($event->sponsors);
-            if ( $sponsors !== null ) {
-              foreach ( $sponsors AS $sponsor ) {
-                foreach ( SponsorshipType::valuesOrdered() as $sponsorType ) {
+          $sponsors = json_decode($event->sponsors);
+          if ($sponsors !== null) {
+          ?>
+            <div class="d-flex justify-content-start align-items-stretch">
+              <?php
+              foreach (SponsorshipType::valuesOrdered() as $sponsorType) {
+                foreach ($sponsors as $sponsor) {
                   $s = $allSponsors->GetSponsorById($sponsor);
-                  if ( $s->type != $sponsorType) continue;
-                  
-                  $type = match((int)$s->type) {
+                  if ($s->type != $sponsorType) continue;
+
+                  $order++;
+
+                  $type = match ((int)$s->type) {
                     SponsorshipType::Legacy_Master->value => 'Legacy Master',
                     SponsorshipType::Legacy_Sustaining->value => 'Legacy Sustaining',
                     SponsorshipType::Master->value => 'Master',
                     SponsorshipType::Sustaining->value => 'Sustaining',
                     default => ''
                   };
-          
-                  if ( $type != '' ):
-                  ?>
-                    <div class="text-center" style="font-size: smaller; color:#ffae00"><?=$type?>&nbsp;Sponsor<br/><?= $allSponsors->GetSmallImageLink($sponsor) ?></div>
+
+                  if ($type != '') :
+              ?>
+                    <div class="border border-danger rounded pt-1 pb-1 pe-2 ps-2 me-1 order-<?= $order ?>">
+                      <p class="text-center tight"><?= $s->name ?><br/>
+                      <span style="font-size: smaller; color:#ffae00"><?= $type ?></span></p>
+                    </div>
                   <?php
+                  else :
+                  ?>
+                    <div class="border border-warning rounded p-1 me-1 order-<?= $order ?>">
+                      <p class="text-center tight"><?= $s->name ?></p>
+                    </div>
+              <?php
                   endif;
                 }
               }
-            } else {
-              echo '&nbsp;';
-            }
+              ?>
+            </div>
+          <?php
+          } else {
+            echo '&nbsp;';
+          }
           ?>
         </td>
       </tr>
