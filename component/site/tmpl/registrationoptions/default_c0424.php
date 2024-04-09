efe6a5e74f6cdfcc368bcd136649cb344bd08750
muddygs <muddygs@gmail.com>
2024-04-08 20:22:28 -0400

    Fixed link if not registered.

--- component/site/tmpl/registrationoptions/default_c0424.php
+++ component/site/tmpl/registrationoptions/default_c0424.php
@@ -122,7 +122,7 @@ $mainEvent = $registrant->getMainEvent();
 
 if ($addons == true && $mainEvent == null && !$vipRedirect) :
 ?>
-  <p class="text-warning">You are not currently registered. Please start your registration <a href="/registration-survey">here</a>.</p>
+  <p class="text-warning">You are not currently registered. Please start your registration <a href="/registration-survey-claw">here</a>.</p>
   <p><span class="fa fa-info-circle fa-2x"></span><a href="/help?category_id=11">&nbsp;Contact Guest Services for assistance.</a></p>
 <?php
   return;
