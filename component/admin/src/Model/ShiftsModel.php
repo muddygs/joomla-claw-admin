8b397fe7c7002dd02911ef30b67f8b2f1008e135
muddygs <muddygs@gmail.com>
2024-05-24 21:14:39 -0400

    No code changes: tab->space formatting

--- component/admin/src/Model/ShiftsModel.php
+++ component/admin/src/Model/ShiftsModel.php
@@ -22,26 +22,26 @@ use Joomla\CMS\MVC\Model\ListModel;
  */
 class ShiftsModel extends ListModel
 {
-	private array $list_fields = [
+  private array $list_fields = [
     'id',
     'published',
     'title',
     'coordinators',
     'shift_area',
-		'event',
+    'event',
   ];	
 
-	/**
-	 * Constructor.
-	 *
-	 * @param   array  $config  An optional associative array of configuration settings.
-	 *
-	 * @see     \JController
-	 * @since   1.6
-	 */
-	public function __construct($config = array())
-	{
-		if (empty($config['filter_fields'])) {
+  /**
+   * Constructor.
+   *
+   * @param   array  $config  An optional associative array of configuration settings.
+   *
+   * @see     \JController
+   * @since   1.6
+   */
+  public function __construct($config = array())
+  {
+    if (empty($config['filter_fields'])) {
       $config['filter_fields'] = [];
       
       foreach( $this->list_fields AS $f )
@@ -51,119 +51,116 @@ class ShiftsModel extends ListModel
       }
     }
 
-		parent::__construct($config);
-	}
+    parent::__construct($config);
+  }
 
-	/**
-	 * Method to auto-populate the model state.
-	 *
-	 * This method should only be called once per instantiation and is designed
-	 * to be called on the first call to the getState() method unless the model
-	 * configuration flag to ignore the request is set.
-	 *
-	 * Note. Calling getState in this method will result in recursion.
-	 *
-	 * @param   string  $ordering   An optional ordering field.
-	 * @param   string  $direction  An optional direction (asc|desc).
-	 *
-	 * @return  void
-	 *
-	 * @since   3.0.1
-	 */
-	protected function populateState($ordering = 'title', $direction = 'ASC')
-	{
-		$app = Factory::getApplication();
+  /**
+   * Method to auto-populate the model state.
+   *
+   * This method should only be called once per instantiation and is designed
+   * to be called on the first call to the getState() method unless the model
+   * configuration flag to ignore the request is set.
+   *
+   * Note. Calling getState in this method will result in recursion.
+   *
+   * @param   string  $ordering   An optional ordering field.
+   * @param   string  $direction  An optional direction (asc|desc).
+   *
+   * @return  void
+   *
+   * @since   3.0.1
+   */
+  protected function populateState($ordering = 'title', $direction = 'ASC')
+  {
+    $app = Factory::getApplication();
 
-		// List state information
-		$value = $app->input->get('limit', $app->get('list_limit', 0), 'uint');
-		$this->setState('list.limit', $value);
+    // List state information
+    $value = $app->input->get('limit', $app->get('list_limit', 0), 'uint');
+    $this->setState('list.limit', $value);
 
-		$value = $app->input->get('limitstart', 0, 'uint');
-		$this->setState('list.start', $value);
+    $value = $app->input->get('limitstart', 0, 'uint');
+    $this->setState('list.start', $value);
 
-		$search = $this->getUserStateFromRequest($this->context . '.filter.search', 'filter_search');
-		$this->setState('filter.search', $search);
+    $search = $this->getUserStateFromRequest($this->context . '.filter.search', 'filter_search');
+    $this->setState('filter.search', $search);
 
-		// List state information.
-		parent::populateState($ordering, $direction);
-	}
+    // List state information.
+    parent::populateState($ordering, $direction);
+  }
 
-	/**
-	 * Method to get a store id based on model configuration state.
-	 *
-	 * This is necessary because the model is used by the component and
-	 * different modules that might need different sets of data or different
-	 * ordering requirements.
-	 *
-	 * @param   string  $id  A prefix for the store id.
-	 *
-	 * @return  string  A store id.
-	 *
-	 * @since   1.6
-	 */
-	protected function getStoreId($id = '')
-	{
-		// Compile the store id.
-		$id .= ':' . serialize($this->getState('filter.name'));
-		$id .= ':' . $this->getState('filter.search');
-		$id .= ':' . $this->getState('filter.state');
+  /**
+   * Method to get a store id based on model configuration state.
+   *
+   * This is necessary because the model is used by the component and
+   * different modules that might need different sets of data or different
+   * ordering requirements.
+   *
+   * @param   string  $id  A prefix for the store id.
+   *
+   * @return  string  A store id.
+   *
+   * @since   1.6
+   */
+  protected function getStoreId($id = '')
+  {
+    // Compile the store id.
+    $id .= ':' . serialize($this->getState('filter.name'));
+    $id .= ':' . $this->getState('filter.search');
+    $id .= ':' . $this->getState('filter.state');
 
-		$id .= ':' . $this->getState('filter.event');
+    $id .= ':' . $this->getState('filter.event');
     $id .= ':' . $this->getState('filter.shift_area');
 
-		return parent::getStoreId($id);
-	}
+    return parent::getStoreId($id);
+  }
 
-	/**
-	 * Get the master query for retrieving a list of Shifts.
-	 *
-	 * @return  \Joomla\Database\DatabaseQuery
-	 *
-	 * @since   1.6
-	 */
-	protected function getListQuery()
-	{
-		// Create a new query object.
-		$db    = $this->getDatabase();
-		$query = $db->getQuery(true);
+  /**
+   * Get the master query for retrieving a list of Shifts.
+   *
+   * @return  \Joomla\Database\DatabaseQuery
+   *
+   * @since   1.6
+   */
+  protected function getListQuery()
+  {
+    // Create a new query object.
+    $db    = $this->getDatabase();
+    $query = $db->getQuery(true);
 
-		// Select the required fields from the table.
-		$query->select(
+    // Select the required fields from the table.
+    $query->select(
       $this->getState(
         'list.select', array_map( function($a) use($db) { return $db->quoteName('a.'.$a); }, $this->list_fields)
       )
     )
       ->from($db->quoteName('#__claw_shifts', 'a'));
 
-		// Filter by search in title.
-		$search = $this->getState('filter.search');
+    // Filter by search in title.
+    $search = $this->getState('filter.search');
 
-		if (!empty($search))
-		{
-			$search = $db->quote('%' . str_replace(' ', '%', $db->escape(trim($search), true) . '%'));
-			$query->where('(a.title LIKE ' . $search . ')');
-		}
+    if (!empty($search)) {
+      $search = $db->quote('%' . str_replace(' ', '%', $db->escape(trim($search), true) . '%'));
+      $query->where('(a.title LIKE ' . $search . ')');
+    }
 
     $event = $this->getState('filter.event', Aliases::current());
 
-		if ( $event != 'all' ) {
+    if ($event != 'all') {
       $query->where('a.event = :event')->bind(':event', $event);
+
+      $shiftArea = $this->getState('filter.shift_area');
+
+      if ($shiftArea != '') {
+        $query->where('a.shift_area = :area')
+        ->bind(':area', $shiftArea);
+      }
     }
 
-		$shiftArea = $this->getState('filter.shift_area');
+    // Add the list ordering clause.
+    $orderCol  = $this->getState('list.ordering', 'a.name');
+    $orderDirn = $this->getState('list.direction', 'ASC');
 
-		if ( $shiftArea != '' )
-		{
-			$query->where('a.shift_area = :area')
-			->bind(':area', $shiftArea);
-		}
-
-
-		// Add the list ordering clause.
-		$orderCol  = $this->getState('list.ordering', 'a.name');
-		$orderDirn = $this->getState('list.direction', 'ASC');
-
-		$query->order($db->escape($orderCol) . ' ' . $db->escape($orderDirn));
-		return $query;
-	}
+    $query->order($db->escape($orderCol) . ' ' . $db->escape($orderDirn));
+    return $query;
+  }
 }
