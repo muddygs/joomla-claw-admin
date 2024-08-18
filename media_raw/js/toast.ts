037e17e0ef939ead32c42f4f3ee4889834ce801b
muddygs <muddygs@gmail.com>
2024-08-18 16:06:13 -0400

    Refactor: removed unused vars and commented out code

--- media_raw/js/toast.ts
+++ media_raw/js/toast.ts
@@ -1,22 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var EBBaseAjaxUrl = "$baseAjaxUrl";
-  var getUrlParameter = function getUrlParameter(sParam: string) {
-    var sPageURL = window.location.search.substring(1),
-      sURLVariables = sPageURL.split("&"),
-      sParameterName,
-      i;
-
-    for (i = 0; i < sURLVariables.length; i++) {
-      sParameterName = sURLVariables[i].split("=");
-
-      if (sParameterName[0] === sParam) {
-        return sParameterName[1] === undefined
-          ? true
-          : decodeURIComponent(sParameterName[1]);
-      }
-    }
-  };
-
   var registerButtons = [].slice.call(
     document.querySelectorAll(
       ".eb-register-button:not(.eb-colorbox-addcart):not(.eb-join-waiting-list-individual-button"
@@ -29,6 +11,7 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
 function cartToast() {
   var toastElList = [].slice.call(document.querySelectorAll('.toast'))
 
@@ -37,21 +20,3 @@ function cartToast() {
   });
   toastList.forEach(toast => toast.show());
 }
-  
-// function cartAdd(eventId: number) {
-
-// }
-
-// function updateCart() {
-//   const cartDiv = document.getElementById('cart_result');
-//   if (cartDiv == null) return;
-
-//   var url = '/index.php?option=com_eventbooking&view=cart&layout=module&format=raw';
-//   fetch(url )
-//   .then(result => result.text())
-//     .then(html => {
-//       const cartDiv = document.getElementById('cart_result');
-//       if ( cartDiv !== null ) cartDiv.innerHTML = html; 
-//   });
-
-// }
