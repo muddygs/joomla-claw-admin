811b05f8594c9a6f7da1ceecf78b1f528e777aaa
muddygs <muddygs@gmail.com>
2024-08-20 08:13:37 -0400

    Refactor: removed deprecated fields (now handled via EventInfo)

--- library/Enums/ConfigFieldNames.php
+++ library/Enums/ConfigFieldNames.php
@@ -1,15 +1,13 @@
 <?php
+
 namespace ClawCorpLib\Enums;
 
 enum ConfigFieldNames: int
 {
-  case CONFIG_COMBO_EVENTS = 1;
   case CONFIG_DEBUG_EMAIL = 2;
   case CONFIG_IMAGES = 3;
   case CONFIG_OVERLAP_CATEGORY = 4;
-  case CONFIG_TIMEZONE = 5;
   case CONFIG_URLPREFIX = 6;
-  case SHIFT_SHIFT_AREA = 7;
   case SKILL_CATEGORY = 8;
   case SKILL_CLASS_TYPE = 9;
   case SKILL_TIME_SLOT = 10;
@@ -18,13 +16,10 @@ enum ConfigFieldNames: int
   public function toString(): string
   {
     return match ($this) {
-      ConfigFieldNames::CONFIG_COMBO_EVENTS => 'config_combo_events',
       ConfigFieldNames::CONFIG_DEBUG_EMAIL => 'config_debug_email',
       ConfigFieldNames::CONFIG_IMAGES => 'config_images',
       ConfigFieldNames::CONFIG_OVERLAP_CATEGORY => 'config_overlap_category',
-      ConfigFieldNames::CONFIG_TIMEZONE => 'config_timezone',
       ConfigFieldNames::CONFIG_URLPREFIX => 'config_urlprefix',
-      ConfigFieldNames::SHIFT_SHIFT_AREA => 'shift_shift_area',
       ConfigFieldNames::SKILL_CATEGORY => 'skill_category',
       ConfigFieldNames::SKILL_CLASS_TYPE => 'skill_class_type',
       ConfigFieldNames::SKILL_TIME_SLOT => 'skill_time_slot',
@@ -34,45 +29,27 @@ enum ConfigFieldNames: int
 
   public static function fromString(string $str): ?ConfigFieldNames
   {
-    switch (strtoupper($str)) {
-      case 'CONFIG_COMBO_EVENTS':
-        return self::CONFIG_COMBO_EVENTS;
-      case 'CONFIG_DEBUG_EMAIL':
-        return self::CONFIG_DEBUG_EMAIL;
-      case 'CONFIG_IMAGES':
-        return self::CONFIG_IMAGES;
-      case 'CONFIG_OVERLAP_CATEGORY':
-        return self::CONFIG_OVERLAP_CATEGORY;
-      case 'CONFIG_TIMEZONE':
-        return self::CONFIG_TIMEZONE;
-      case 'CONFIG_URLPREFIX':
-        return self::CONFIG_URLPREFIX;
-      case 'SHIFT_SHIFT_AREA':
-        return self::SHIFT_SHIFT_AREA;
-      case 'SKILL_CATEGORY':
-        return self::SKILL_CATEGORY;
-      case 'SKILL_CLASS_TYPE':
-        return self::SKILL_CLASS_TYPE;
-      case 'SKILL_TIME_SLOT':
-        return self::SKILL_TIME_SLOT;
-      case 'SKILL_TRACK':
-        return self::SKILL_TRACK;
-      default:
-        return null;
-    }
+    return match (strtoupper($str)) {
+      'CONFIG_DEBUG_EMAIL' => self::CONFIG_DEBUG_EMAIL,
+      'CONFIG_IMAGES' => self::CONFIG_IMAGES,
+      'CONFIG_OVERLAP_CATEGORY' => self::CONFIG_OVERLAP_CATEGORY,
+      'CONFIG_URLPREFIX' => self::CONFIG_URLPREFIX,
+      'SKILL_CATEGORY' => self::SKILL_CATEGORY,
+      'SKILL_CLASS_TYPE' => self::SKILL_CLASS_TYPE,
+      'SKILL_TIME_SLOT' => self::SKILL_TIME_SLOT,
+      'SKILL_TRACK' => self::SKILL_TRACK,
+      default => null,
+    };
   }
 
   public static function toOptions(): array
   {
     $result = [];
 
-    foreach ( ConfigFieldNames::cases() as $c ) {
+    foreach (ConfigFieldNames::cases() as $c) {
       $result[$c->value] = $c->toString();
     }
 
     return $result;
   }
-
 }
-
-
