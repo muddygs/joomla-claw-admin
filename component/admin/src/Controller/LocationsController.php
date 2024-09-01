de91a5fc837a6682cff3c4a6949cc800d9a97d97
muddygs <muddygs@gmail.com>
2024-09-01 18:30:56 -0400

    Add: Needed for delete routing to work

---
+++ component/admin/src/Controller/LocationsController.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * @package     ClawCorp
+ * @subpackage  com_claw
+ *
+ * @copyright   (C) 2024 C.L.A.W. Corp. All Rights Reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+
+namespace ClawCorp\Component\Claw\Administrator\Controller;
+
+defined('_JEXEC') or die;
+
+use ClawCorpLib\Traits\Controller;
+use Joomla\CMS\MVC\Controller\AdminController;
+use Joomla\CMS\Application\CMSApplication;
+use Joomla\CMS\Form\FormFactoryInterface;
+use Joomla\CMS\MVC\Factory\MVCFactoryInterface;
+use Joomla\Input\Input;
+
+/**
+ * Locations list controller class.
+ *
+ */
+class LocationsController extends AdminController
+{
+  use Controller;
+
+  public function __construct(
+    $config = [],
+    MVCFactoryInterface $factory = null,
+    ?CMSApplication $app = null,
+    ?Input $input = null,
+    FormFactoryInterface $formFactory = null
+  ) {
+    parent::__construct($config, $factory, $app, $input, $formFactory);
+
+    $this->controllerSetup();
+  }
+}
