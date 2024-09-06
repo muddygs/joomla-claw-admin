0dd3e03b8e744d6229c22c2f194250dcbcf9ea52
muddygs <muddygs@gmail.com>
2024-09-05 21:42:27 -0400

    Refactory: use "nav-justified" instead of "nav-full" in pill tabs

--- library/Helpers/Bootstrap.php
+++ library/Helpers/Bootstrap.php
@@ -20,11 +20,11 @@ class Bootstrap
 
   public static function writePillTabList(array $tabTitles, string $activeTab = ''): string
   {
-    $guid = substr(str_shuffle('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'),1,8);
+    $guid = substr(str_shuffle('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'), 1, 8);
 
     if ($activeTab == '') $activeTab = $tabTitles[0];
 ?>
-    <ul class="nav nav-pills nav-fill mb-3" id="pills-tab-<?php echo $guid ?>" role="tablist">
+    <ul class="nav nav-pills nav-justified mb-3" id="pills-tab-<?php echo $guid ?>" role="tablist">
       <?php
       foreach ($tabTitles as $title) {
         $active = $title == $activeTab ? 'active' : '';
@@ -82,17 +82,17 @@ class Bootstrap
   {
     $guid = UserHelper::genRandomPassword(8);
     $intervalMS = $intervalSeconds * 1000;
-?>
-<div id="<?= $guid ?>" class="carousel slide carousel-fade" data-bs-ride="carousel" data-bs-config='{"pause":false}'>
-  <div class="carousel-inner">
-    <?php foreach ($carouselContents as $i => $content) : ?>
-      <div class="carousel-item <?= $i == 0 ? 'active' : '' ?>" data-bs-interval="<?= $intervalMS?>">
-        <?= $content ?>
+    ?>
+    <div id="<?= $guid ?>" class="carousel slide carousel-fade" data-bs-ride="carousel" data-bs-config='{"pause":false}'>
+      <div class="carousel-inner">
+        <?php foreach ($carouselContents as $i => $content) : ?>
+          <div class="carousel-item <?= $i == 0 ? 'active' : '' ?>" data-bs-interval="<?= $intervalMS ?>">
+            <?= $content ?>
+          </div>
+        <?php endforeach; ?>
       </div>
-    <?php endforeach; ?>
-  </div>
-</div>
-<?php
+    </div>
+  <?php
   }
 
   /**
@@ -129,7 +129,7 @@ class Bootstrap
   public static function rawHeader(array $js = [], array $css = [])
   {
     $ts = 'ts=' . time();
-    ?>
+  ?>
     <!DOCTYPE html>
     <html lang="en">
 
@@ -140,29 +140,30 @@ class Bootstrap
       <meta http-equiv="Expires" content="0" />
       <title>READY TO PRINT</title>
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <?php
-      foreach ( $js AS $j ) {
+      <?php
+      foreach ($js as $j) {
         echo "<script src=\"$j?$ts\"></script>\n";
       }
-    ?>
+      ?>
       <link href="/templates/shaper_helixultimate/css/font-awesome.min.css" rel="stylesheet" />
       <?php
-      foreach ( $css AS $c ) {
+      foreach ($css as $c) {
         echo "<link href=\"$c?$ts\" rel=\"stylesheet\" />\n";
       }
-    ?>
+      ?>
     </head>
 
     <body>
-  <?php
+    <?php
   }
 
   public static function rawFooter()
   {
     ?>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     </body>
+
     </html>
-    <?php
+<?php
   }
 }
