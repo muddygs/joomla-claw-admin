2989502cbd394ee1828b81853ce1ab2f8fdad855
maddog <maddog@cybernet.com>
2024-09-16 21:32:29 -0400

    Reformat: via conform

--- component/admin/tmpl/schedule/edit.php
+++ component/admin/tmpl/schedule/edit.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @package     ClawCorp
  * @subpackage  com_claw
@@ -10,9 +11,7 @@
 defined('_JEXEC') or die;
 
 use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Layout\LayoutHelper;
 use Joomla\CMS\Router\Route;
-use Joomla\CMS\Form\Form;
 
 HTMLHelper::_('behavior.formvalidator');
 HTMLHelper::_('behavior.keepalive');
@@ -21,50 +20,50 @@ $view = 'schedule';
 
 ?>
 
-<form action="<?php echo Route::_('index.php?option=com_claw&view='.$view.'&layout=edit&id=' . (int) $this->item->id); ?>"
-	method="post" name="adminForm" id="<?php echo $view ?>-form" class="form-validate">
+<form action="<?php echo Route::_('index.php?option=com_claw&view=' . $view . '&layout=edit&id=' . (int) $this->item->id); ?>"
+  method="post" name="adminForm" id="<?php echo $view ?>-form" class="form-validate">
 
-	<div class="row form-vertical mb-3">
+  <div class="row form-vertical mb-3">
     <div class="col-12 col-md-8">
-        <?php echo $this->form->renderField('event_title'); ?>
+      <?php echo $this->form->renderField('event_title'); ?>
     </div>
     <div class="col-12 col-md-4">
-        <?php echo $this->form->renderField('published'); ?>
+      <?php echo $this->form->renderField('published'); ?>
     </div>
-	</div>
+  </div>
 
-	<div>
-		<?php echo HTMLHelper::_('uitab.startTabSet', 'myTab', ['active' => 'general']); ?>
+  <div>
+    <?php echo HTMLHelper::_('uitab.startTabSet', 'myTab', ['active' => 'general']); ?>
 
-		<?php echo HTMLHelper::_('uitab.addTab', 'myTab', 'general', 'General'); ?>
-		<div class="row">
-			<?php echo $this->form->renderField('event'); ?>
-			<?php echo $this->form->renderField('day'); ?>
-			<?php echo $this->form->renderField('start_time'); ?>
-			<?php echo $this->form->renderField('end_time'); ?>
-			<?php echo $this->form->renderField('featured'); ?>
-			<?php echo $this->form->renderField('location'); ?>
-			<?php echo $this->form->renderField('event_description'); ?>
-			<?php echo $this->form->renderField('sponsors'); ?>
-
-
-		</div>
-		<?php echo HTMLHelper::_('uitab.endTab'); ?>
-
-		<?php echo HTMLHelper::_('uitab.addTab', 'myTab', 'special', 'Special'); ?>
-			<?php echo $this->form->renderField('fee_event'); ?>
-			<?php echo $this->form->renderField('event_id'); ?>
-			<?php echo $this->form->renderField('onsite_description'); ?>
-			<?php echo $this->form->renderField('poster'); ?>
+    <?php echo HTMLHelper::_('uitab.addTab', 'myTab', 'general', 'General'); ?>
+    <div class="row">
+      <?php echo $this->form->renderField('event'); ?>
+      <?php echo $this->form->renderField('day'); ?>
+      <?php echo $this->form->renderField('start_time'); ?>
+      <?php echo $this->form->renderField('end_time'); ?>
+      <?php echo $this->form->renderField('featured'); ?>
+      <?php echo $this->form->renderField('location'); ?>
+      <?php echo $this->form->renderField('event_description'); ?>
+      <?php echo $this->form->renderField('sponsors'); ?>
 
 
-		<?php echo HTMLHelper::_('uitab.endTab'); ?>
+    </div>
+    <?php echo HTMLHelper::_('uitab.endTab'); ?>
 
-		<?php echo HTMLHelper::_('uitab.endTabSet'); ?>
-	</div>
-	
-	<input type="hidden" name="id" value="<?php echo $this->item->id ?>"/>
-	<input type="hidden" name="sort_order" value="<?php echo $this->item->sort_order ?>"/>
-	<input type="hidden" name="task" value=""/>
-	<?php echo HTMLHelper::_('form.token'); ?>
+    <?php echo HTMLHelper::_('uitab.addTab', 'myTab', 'special', 'Special'); ?>
+    <?php echo $this->form->renderField('fee_event'); ?>
+    <?php echo $this->form->renderField('event_id'); ?>
+    <?php echo $this->form->renderField('onsite_description'); ?>
+    <?php echo $this->form->renderField('poster'); ?>
+
+
+    <?php echo HTMLHelper::_('uitab.endTab'); ?>
+
+    <?php echo HTMLHelper::_('uitab.endTabSet'); ?>
+  </div>
+
+  <input type="hidden" name="id" value="<?php echo $this->item->id ?>" />
+  <input type="hidden" name="sort_order" value="<?php echo $this->item->sort_order ?>" />
+  <input type="hidden" name="task" value="" />
+  <?php echo HTMLHelper::_('form.token'); ?>
 </form>
