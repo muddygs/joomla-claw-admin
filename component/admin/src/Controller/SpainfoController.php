58ec0c18b21c89c5d00f3ee180355422f32eaa6b
muddygs <muddygs@gmail.com>
2024-09-24 07:56:39 -0400

    Refactor: fall back to default controller only

--- component/admin/src/Controller/SpainfoController.php
+++ component/admin/src/Controller/SpainfoController.php
@@ -25,17 +25,5 @@ use Joomla\Input\Input;
  */
 class SpainfoController extends FormController
 {
-  use Controller;
 
-  public function __construct(
-    $config = [],
-    MVCFactoryInterface $factory = null,
-    ?CMSApplication $app = null,
-    ?Input $input = null,
-    FormFactoryInterface $formFactory = null
-  ) {
-    parent::__construct($config, $factory, $app, $input, $formFactory);
-
-    $this->controllerSetup();
-  }
 }
