09aa66f0048d2ab8a8fa0323bf21521d1350a625
muddygs <muddygs@gmail.com>
2024-10-18 22:49:23 -0400

    Add: plugin/task sync from working space to git working dir

--- rsync.sh
+++ rsync.sh
@@ -5,7 +5,7 @@ rsync -arv /var/www/j4_dev/public_html/components/com_claw/ component/site/
 mv component/admin/claw.xml component/
 #rsync -arv --delete /var/www/j4_dev/public_html/libraries/claw/ library/
 rsync -arv /var/www/j4_dev/public_html/libraries/claw/ library/
-#rsync -arv /var/www/j4_dev/public_html/plugins/system/claw/ plugin/
+rsync -arv /var/www/j4_dev/public_html/plugins/task/clawcorp/ plugins/task/clawcorp/
 rsync -arv /var/www/j4_dev/public_html/media/com_claw/ media_raw/
 rsync -arv /var/www/j4_dev/public_html/modules/mod_claw_sponsors modules/
 rsync -arv /var/www/j4_dev/public_html/modules/mod_claw_cart modules/
