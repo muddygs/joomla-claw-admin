fbb051f584985d31b034f94314b8ef75d41d800c
muddygs <muddygs@gmail.com>
2024-10-20 20:54:53 -0400

    Chore: conform reformat

--- component/admin/src/View/Skill/HtmlView.php
+++ component/admin/src/View/Skill/HtmlView.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @package     ClawCorp
  * @subpackage  com_claw
@@ -18,58 +19,57 @@ use Joomla\CMS\Toolbar\ToolbarHelper;
 
 class HtmlView extends BaseHtmlView
 {
-	/**
-	 * @param   string  $tpl  The name of the template file to parse; automatically searches through the template paths.
-	 * @return  void
-	 */
-	function display($tpl = null)
-	{
-		$this->state = $this->get('State');
-		$this->form  = $this->get('Form');
-		$this->item  = $this->get('Item');
+  /**
+   * @param   string  $tpl  The name of the template file to parse; automatically searches through the template paths.
+   * @return  void
+   */
+  function display($tpl = null)
+  {
+    $this->state = $this->get('State');
+    $this->form  = $this->get('Form');
+    $this->item  = $this->get('Item');
 
-		// Check for errors.
-		if (count($errors = $this->get('Errors'))) {
-			throw new GenericDataException(implode("\n", $errors), 500);
-		}
+    // Check for errors.
+    if (count($errors = $this->get('Errors'))) {
+      throw new GenericDataException(implode("\n", $errors), 500);
+    }
 
-		$this->addToolbar();
+    $this->addToolbar();
 
-		parent::display($tpl);
-	}
+    parent::display($tpl);
+  }
 
-	/**
-	 * Add the page title and toolbar.
-	 *
-	 * @return  void
-	 *
-	 * @throws \Exception
-	 * @since   1.6
-	 */
-	protected function addToolbar()
-	{
-		Factory::getApplication()->input->set('hidemainmenu', true);
-		$isNew      = ($this->item->id == 0);
+  /**
+   * Add the page title and toolbar.
+   *
+   * @return  void
+   *
+   * @throws \Exception
+   * @since   1.6
+   */
+  protected function addToolbar()
+  {
+    Factory::getApplication()->input->set('hidemainmenu', true);
+    $isNew      = ($this->item->id == 0);
 
-		ToolbarHelper::title(
-			'CLAW Skills Class ' . ($isNew ? 'Add' : 'Edit')
-		);
+    ToolbarHelper::title(
+      'CLAW Skills Class ' . ($isNew ? 'Add' : 'Edit')
+    );
 
-		$user = Factory::getApplication()->getIdentity();
+    $user = Factory::getApplication()->getIdentity();
 
-		if ( $user->authorise('claw.skills', 'com_claw') ) {
-			ToolbarHelper::apply('skill.apply');
-			ToolbarHelper::save('skill.save');
-			ToolbarHelper::save('skill.save2copy','Copy Class');
-		}
+    if ($user->authorise('claw.skills', 'com_claw')) {
+      ToolbarHelper::apply('skill.apply');
+      ToolbarHelper::save('skill.save');
+      ToolbarHelper::save('skill.save2copy', 'Copy Class');
+    }
 
-		if ($isNew) {
-			ToolbarHelper::cancel('skill.cancel');
-		} else {
-			ToolbarHelper::cancel('skill.cancel', 'JTOOLBAR_CLOSE');
-		}
+    if ($isNew) {
+      ToolbarHelper::cancel('skill.cancel');
+    } else {
+      ToolbarHelper::cancel('skill.cancel', 'JTOOLBAR_CLOSE');
+    }
 
-		ToolbarHelper::divider();
-	}
-
+    ToolbarHelper::divider();
+  }
 }
