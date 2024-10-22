989595ef68d744cb63ffb1b90889e2b2e591cb37
muddygs <muddygs@gmail.com>
2024-10-21 23:40:41 -0400

    Fix: variable typo

--- component/site/tmpl/registrationoptions/default_alreadyregistered.php
+++ component/site/tmpl/registrationoptions/default_alreadyregistered.php
@@ -1,2 +1,2 @@
-<p class="text-warning">You cannot register for this event because you are already registered for <b><?= $mainEvent->event->title ?></b>.</p>
+<p class="text-warning">You cannot register for this event because you are already registered for <b><?= $this->mainEvent->event->title ?></b>.</p>
 <p><span class="fa fa-info-circle fa-2x"></span><a href="/help?category_id=11">&nbsp;Contact Guest Services for assistance.</a></p>
