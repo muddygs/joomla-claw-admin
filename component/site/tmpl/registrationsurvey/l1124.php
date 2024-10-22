b8d4628040250476316b97d28ef49c98ca25f69f
muddygs <muddygs@gmail.com>
2024-10-21 23:39:56 -0400

    Fix: do not display reg tabs if $this->mainEvent not null (registered)

--- component/site/tmpl/registrationsurvey/l1124.php
+++ component/site/tmpl/registrationsurvey/l1124.php
@@ -9,29 +9,32 @@ $this->setLayout('common');
 echo $this->loadTemplate('heading');
 $this->setLayout($eventLayout);
 
+if (is_null($this->mainEvent)):
 ?>
-<h1>Or Select Registration Type</h1>
+  <h1>Or Select Registration Type</h1>
 
+  <?php
+
+  $tabs = ['Attendee', 'Volunteer', 'VIP'];
+  $html = [];
+  $html[] = $this->loadTemplate('attendee');
+  $html[] = $this->loadTemplate('volunteer');
+  $html[] = $this->loadTemplate('vip');
+
+  if ($this->onsiteActive) {
+    $tabs[] = 'Day Passes';
+    $html[] = $this->loadTemplate('daypasses');
+    $tabs[] = 'Passes';
+    $html[] = $this->loadTemplate('passes');
+  }
+
+  $tabs[] = 'Other';
+  $html[] = $this->loadTemplate('other');
+
+
+  Bootstrap::writePillTabs($tabs, $html, 'none');
+
+  ?>
+  <hr />
 <?php
-
-$tabs = ['Attendee', 'Volunteer', 'VIP'];
-$html = [];
-$html[] = $this->loadTemplate('attendee');
-$html[] = $this->loadTemplate('volunteer');
-$html[] = $this->loadTemplate('vip');
-
-if ($this->onsiteActive) {
-  $tabs[] = 'Day Passes';
-  $html[] = $this->loadTemplate('daypasses');
-  $tabs[] = 'Passes';
-  $html[] = $this->loadTemplate('passes');
-}
-
-$tabs[] = 'Other';
-$html[] = $this->loadTemplate('other');
-
-
-Bootstrap::writePillTabs($tabs, $html, 'none');
-
-?>
-<hr />
+endif;
