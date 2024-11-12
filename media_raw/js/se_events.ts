1c0d4aaa8c4068d472cd39ec9a5941b0ed7281a0
muddygs <muddygs@gmail.com>
2024-11-11 22:35:18 -0500

    Refactor: moved wait for dom ready to RS Form script initialization
      * Copied in the RS Form script as comment

--- media_raw/js/se_events.ts
+++ media_raw/js/se_events.ts
@@ -1,6 +1,33 @@
-document.addEventListener("DOMContentLoaded", function () {
-  updateClassList();
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/** RSFORM SCRIPT
+<script>
+
+var script = document.createElement('script');
+script.src = "/media/com_claw/js/se_events.js";
+
+// Append the script to the head
+document.head.appendChild(script);
+
+// Function to check if `class_start_time` is ready and then call updateClassList
+function checkAndUpdateClassList() {
+    var startTimeElement = document.getElementById('class_start_time');
+
+    if (startTimeElement && typeof updateClassList === 'function') {
+        // `class_start_time` is ready, call updateClassList
+        updateClassList();
+    } else {
+        // If `class_start_time` is not ready, call this function again after 500ms
+        setTimeout(checkAndUpdateClassList, 500);
+    }
+}
+
+window.addEventListener('DOMContentLoaded', function () {
+    setTimeout(checkAndUpdateClassList, 500);
 });
+
+</script>
+
+ */
 
 class classRecord {
   stime: string;
@@ -8,7 +35,7 @@ class classRecord {
   gid: string;
   day: string;
 
-  constructor(stime: string, title:string,gid:string,day:string) {
+  constructor(stime: string, title: string, gid: string, day: string) {
     this.stime = stime;
     this.title = title;
     this.gid = gid;
@@ -24,44 +51,43 @@ function updateClassList() {
 
   let selects: classRecord[] = [];
 
-  if (stime == 'any') {
-    selects = classes
-  } else {
-    classes.forEach((c) => {
-      const t: number = Number(c.stime);
-      const d: number = Number(c.day);
+  classes.forEach((c) => {
+    const t: number = Number(c.stime);
+    const d: number = Number(c.day);
 
-      // Any, FriAM, ..., Sun
+    // Any, FriAM, ..., Sun
 
-      switch (stime) {
-        case 'friam':
-          if (d == 5 && t < 1200) {
-            selects.push(c);
-          }
-          break;
-        case 'fripm':
-          if (d == 5 && t > 1159) {
-            selects.push(c);
-          }
-          break;
-        case 'satam':
-          if (d == 6 && t < 1200) {
-            selects.push(c);
-          }
-          break;
-        case 'satpm':
-          if (d == 6 && t > 1159) {
-            selects.push(c);
-          }
-          break;
-        case 'sun':
-          if (d == 0) {
-            selects.push(c);
-          }
-          break;
-      }
-    });
-  }
+    switch (stime) {
+      case 'friam':
+        if (d == 5 && t < 1200) {
+          selects.push(c);
+        }
+        break;
+      case 'fripm':
+        if (d == 5 && t > 1159) {
+          selects.push(c);
+        }
+        break;
+      case 'satam':
+        if (d == 6 && t < 1200) {
+          selects.push(c);
+        }
+        break;
+      case 'satpm':
+        if (d == 6 && t > 1159) {
+          selects.push(c);
+        }
+        break;
+      case 'sun':
+        if (d == 0) {
+          selects.push(c);
+        }
+        break;
+      default:
+        selects.push(c);
+        break;
+    }
+  });
 
   const classTitleParam = document.getElementById('classTitleParam') as HTMLInputElement;
   let preselect = '';
