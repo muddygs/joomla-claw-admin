970dd47075cdb7a16ff963814c358f802cdb603b
muddygs <muddygs@gmail.com>
2024-11-20 21:42:25 -0500

    Refactor/move: static functions moved into model

--- component/admin/src/Model/ReportsModel.php
+++ component/admin/src/Model/ReportsModel.php
@@ -171,12 +171,12 @@ class ReportsModel extends BaseDatabaseModel
     $items = [];
     $userFactory = new UserFactory($this->getDatabase());
 
-    $shifts = Volunteers::getShiftEventDetails($this->eventConfig->alias);
+    $shifts = self::getShiftEventDetails($this->eventConfig->alias);
 
     $coordinators = [];
 
     foreach (array_keys($shifts) as $sid) {
-      $info = Volunteers::getShiftInfo($sid);
+      $info = self::getShiftInfo($sid);
       if (null == $info) continue;
 
       $primaryCoordinator = $info->coordinators[0];
@@ -189,7 +189,6 @@ class ReportsModel extends BaseDatabaseModel
       ];
     }
 
-
     $items['eventInfo'] = $this->eventConfig->eventInfo;
     $items['shifts'] = $shifts;
     $items['coordinators'] = $coordinators;
@@ -197,6 +196,87 @@ class ReportsModel extends BaseDatabaseModel
     return $items;
   }
 
+  /**
+   * Retrieve shift title and coordinator user ids
+   * @param int $sid Shift ID
+   * @return object Shift information or null
+   */
+  private function getShiftInfo(int $sid): ?object
+  {
+    /** @var \Joomla\Database\DatabaseDriver */
+    $db = $this->getDatabase();
+    $sid = $db->quote($sid);
+
+    $query = $db->getQuery(true);
+    $query->select($db->qn(['title', 'coordinators']))
+      ->from($db->qn('#__claw_shifts'))
+      ->where($db->qn('id') . '=' . $sid);
+
+    $db->setQuery($query);
+    $row = $db->loadObject();
+
+    if (null == $row) return null;
+
+    $row->coordinators = json_decode($row->coordinators);
+
+    return $row;
+  }
+
+  /**
+   * Given a key/value id/title array of shift events, loads the details
+   * based on the event ids
+   * @param string Event alias from which to pull shifts
+   * @return array eb_event records based on input event ids
+   */
+  private function getShiftEventDetails(string $clawEventAlias): array
+  {
+    /** @var \Joomla\Database\DatabaseDriver */
+    $db = $this->getDatabase();
+
+    $eventConfig = new EventConfig($clawEventAlias);
+
+    $shiftEvents = $eventConfig->getEventsByCategoryId(
+      array_merge($eventConfig->eventInfo->eb_cat_shifts, $eventConfig->eventInfo->eb_cat_supershifts)
+    );
+
+    $eventIds = array_column($shiftEvents, 'id');
+
+    if (sizeof($eventIds) == 0) return [];
+
+    $e = implode(',', $eventIds);
+
+    $option = [];
+
+    $query = $db->getQuery(true);
+    $query->select($db->qn(['id', 'title', 'alias', 'event_capacity', 'event_date', 'event_end_date', 'published']))
+      ->select('( SELECT count(*) FROM #__eb_registrants r WHERE r.event_id = e.id AND r.published = 1 ) AS memberCount')
+      ->from($db->qn('#__eb_events', 'e'))
+      ->where($db->qn('id') . ' IN (' . $e . ')')
+      ->order($db->qn('title'));
+
+    $db->setQuery($query);
+    $rows = $db->loadObjectList();
+
+    foreach ($rows as $row) {
+      if (!str_starts_with($row->alias, $eventConfig->eventInfo->shiftPrefix)) continue;
+
+      $pattern = '/-(\d+)-/';
+
+      if (preg_match($pattern, substr($row->alias, strlen($eventConfig->eventInfo->shiftPrefix)), $matches)) {
+        $sid = $matches[1];
+      } else {
+        continue;
+      }
+
+      if (!array_key_exists($sid, $option)) {
+        $option[$sid] = [];
+      }
+
+      $option[$sid][$row->id] = $row;
+    }
+
+    return $option;
+  }
 
   private function findListCustomField(int $categoryId): ?object
   {
