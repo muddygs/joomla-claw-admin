5591dc11100470fc4c396fa36495f0782bc25c65
muddygs <muddygs@gmail.com>
2025-01-01 09:49:32 -0500

    Del: no custom functions needed in this model
      * Probably can delete getDatabase(), but I haven't yet

--- component/site/src/Model/SkillspresenterModel.php
+++ component/site/src/Model/SkillspresenterModel.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @package     ClawCorp
  * @subpackage  com_claw
@@ -12,24 +13,15 @@ namespace ClawCorp\Component\Claw\Site\Model;
 defined('_JEXEC') or die;
 
 use Joomla\CMS\MVC\Model\BaseDatabaseModel;
-use ClawCorpLib\Helpers\Skills;
 
 /**
  * Methods to handle public class listing.
  */
 class SkillsPresenterModel extends BaseDatabaseModel
 {
-  public function GetPresenter(int $pid, string $event): ?object
-  {
-    $db = $this->getDatabase();
-    $skills = new Skills($db, $event);
-    $presenter = $skills->GetPresenter($pid);
-
-    return $presenter;
-  }
-
   public function getDatabase(): \Joomla\Database\DatabaseInterface
   {
     return parent::getDatabase();
   }
 }
+
