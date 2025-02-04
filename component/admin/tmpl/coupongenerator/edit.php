c6f17b8e2d9f1919955a21c233450fbaed928e11
muddygs <muddygs@gmail.com>
2025-02-03 21:29:22 -0500

    Chore: conform format

--- component/admin/tmpl/coupongenerator/edit.php
+++ component/admin/tmpl/coupongenerator/edit.php
@@ -16,7 +16,7 @@ use Joomla\CMS\Session\Session;
 
 /** @var Joomla\CMS\Application\AdministratorApplication */
 $app = Factory::getApplication();
-/** @var Joomla\CMS\WebAsset\WebAssetManager $wa */
+/** @var Joomla\CMS\WebAsset\WebAssetManager */
 $wa = $app->getDocument()->getWebAssetManager();
 $wa->useScript('com_claw.coupongenerator');
 $wa->useScript('htmx');
@@ -27,13 +27,13 @@ $token = Session::getFormToken();
 
 <h1>Coupon Generator</h1>
 
-<form 
-action="#" 
-method="post" 
-name="Coupon Generator" 
-id="claw-coupon-generator" 
-class="row g-3" 
-hx-headers='{"X-CSRF-Token": "<?= $token ?>"}'>
+<form
+  action="#"
+  method="post"
+  name="Coupon Generator"
+  id="claw-coupon-generator"
+  class="row g-3"
+  hx-headers='{"X-CSRF-Token": "<?= $token ?>"}'>
   <div class="col-12">
     <p>Enter quantity and select coupon registration type and any add ons.</p>
   </div>
@@ -60,8 +60,7 @@ hx-headers='{"X-CSRF-Token": "<?= $token ?>"}'>
       <label id="jform_value-lbl" for="jform_value">Total Value</label>
       <div id="total_value"
         hx-post="/administrator/index.php?option=com_claw&task=coupongenerator.couponValue&format=raw"
-        hx-trigger="change from:#jform_event delay:1s, change from:#jform_packageid delay:1s, change from:#jform_addons"
-      >
+        hx-trigger="change from:#jform_event delay:1s, change from:#jform_packageid delay:1s, change from:#jform_addons">
         $0.00
       </div>
     </div>
@@ -72,36 +71,33 @@ hx-headers='{"X-CSRF-Token": "<?= $token ?>"}'>
   <div class="text-end">
     <input name="checkEmail" id="checkEmail" type="button" value="Validate Email(s)" class="btn btn-info w-50"
       hx-post="/administrator/index.php?option=com_claw&task=coupongenerator.emailStatus&format=raw"
-      hx-target="#emailstatus"
-    />
+      hx-target="#emailstatus" />
     <?php if ($this->emailOverride) : ?>
-    <div class="ms-2 form-check float-end">
-      <input class="form-check-input" type="checkbox" value="1" id="emailOverride" name="emailOverride">
-      <label class="form-check-label" for="emailOverride">Ignore email errors</label>
-    </div>
-  <?php endif; ?>
+      <div class="ms-2 form-check float-end">
+        <input class="form-check-input" type="checkbox" value="1" id="emailOverride" name="emailOverride">
+        <label class="form-check-label" for="emailOverride">Ignore email errors</label>
+      </div>
+    <?php endif; ?>
   </div>
   <div id="emailstatus" class="text-info text-end"></div>
 
   <hr />
 
   <div class="row">
-  <div class="col-6">
-    <input name="generate" id="generate" type="button" value="Generate" class="btn btn-danger mb-2" hx-post="/administrator/index.php?option=com_claw&task=coupongenerator.createCoupons&format=raw" hx-target="#results" />
-  </div>
-  <div class="col-6 text-end">
-    <button name="copy" id="copy" class="btn btn-info mb-2" onclick="copyCoupons()"
-      data-bs-toggle="tooltip" data-bs-placement="top" title="Copy to Clipboard"
-    >
-      <span class="fa fa-copy"></span>
-    </button>
-  </div>
+    <div class="col-6">
+      <input name="generate" id="generate" type="button" value="Generate" class="btn btn-danger mb-2" hx-post="/administrator/index.php?option=com_claw&task=coupongenerator.createCoupons&format=raw" hx-target="#results" />
+    </div>
+    <div class="col-6 text-end">
+      <button name="copy" id="copy" class="btn btn-info mb-2" onclick="copyCoupons()"
+        data-bs-toggle="tooltip" data-bs-placement="top" title="Copy to Clipboard">
+        <span class="fa fa-copy"></span>
+      </button>
+    </div>
 
-  <?php echo HTMLHelper::_('form.token'); ?>
+    <?php echo HTMLHelper::_('form.token'); ?>
 </form>
 
 
 <div id="results">
   <p>Ready!</p>
 </div>
-
