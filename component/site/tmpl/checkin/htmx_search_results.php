db63c56981cd2c818a74382ec6cf0337b427139e
muddygs <muddygs@gmail.com>
2025-02-06 20:32:39 -0500

    Fix: html misformat

--- component/site/tmpl/checkin/htmx_search_results.php
+++ component/site/tmpl/checkin/htmx_search_results.php
@@ -42,49 +42,50 @@ if (property_exists($this, 'record')) {
       <div class="row">
         <div class="col-4">Package Type</div>
         <div class="col-8 fw-bold" id="clawPackage" name="info" style="color:#ffae00">
-          <<?= $record->clawPackage ?> /div>
+          <?= $record->clawPackage ?>
         </div>
       </div>
-    </div>
-
-    <div class="row mb-1">
-      <div class="col-6 border border-danger py-2">
-        <div class="row">
-          <div class="col-4">City</div>
-          <div class="col-8 fw-bold" id="city" name="info" style="color:#ffae00"><?= $record->city ?></div>
-        </div>
-      </div>
-      <div class="col-6 border border-danger py-2">
-        <div class="row">
-          <div class="col-4">Shirt Size</div>
-          <div class="col-8 fw-bold" id="shirtSize" name="info" style="color:#ffae00"><?= $record->shirtSize ?></div>
-        </div>
-      </div>
-    </div>
-
-    <div class="row">
-      <div class="col-4 border border-danger py-2">
-        <div class="row">
-          <div class="col-4">Dinner</div>
-          <div class="col-8 fw-bold" id="dinner" name="info" style="color:#ffae00"><?= $record->dinner ?></div>
-        </div>
-      </div>
-      <div class="col-4 border border-danger py-2">
-        <div class="row">
-          <div class="col-4">Brunch</div>
-          <div class="col-8 fw-bold" id="brunch" name="info" style="color:#ffae00"><?= $record->brunch ?></div>
-        </div>
-      </div>
-      <div class="col-4 border border-danger py-2">
-        <div class="row">
-          <div class="col-4">Buffets</div>
-          <div class="col-8 fw-bold" id="buffets" name="info" style="color:#ffae00" colspan="3"><?= $record->buffets ?></div>
-        </div>
-      </div>
-    </div>
-
-    <div class="row border border-danger py-2 mt-1">
-      <div class="col-2">Volunteer Shifts</div>
-      <div class="col-10" id="shifts" name="info" style="color:#ffae00" colspan="3"><?= $record->shifts ?></div>
     </div>
   </div>
+
+  <div class="row mb-1">
+    <div class="col-6 border border-danger py-2">
+      <div class="row">
+        <div class="col-4">City</div>
+        <div class="col-8 fw-bold" id="city" name="info" style="color:#ffae00"><?= $record->city ?></div>
+      </div>
+    </div>
+    <div class="col-6 border border-danger py-2">
+      <div class="row">
+        <div class="col-4">Shirt Size</div>
+        <div class="col-8 fw-bold" id="shirtSize" name="info" style="color:#ffae00"><?= $record->shirtSize ?></div>
+      </div>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-4 border border-danger py-2">
+      <div class="row">
+        <div class="col-4">Dinner</div>
+        <div class="col-8 fw-bold" id="dinner" name="info" style="color:#ffae00"><?= $record->dinner ?></div>
+      </div>
+    </div>
+    <div class="col-4 border border-danger py-2">
+      <div class="row">
+        <div class="col-4">Brunch</div>
+        <div class="col-8 fw-bold" id="brunch" name="info" style="color:#ffae00"><?= $record->brunch ?></div>
+      </div>
+    </div>
+    <div class="col-4 border border-danger py-2">
+      <div class="row">
+        <div class="col-4">Buffets</div>
+        <div class="col-8 fw-bold" id="buffets" name="info" style="color:#ffae00" colspan="3"><?= $record->buffets ?></div>
+      </div>
+    </div>
+  </div>
+
+  <div class="row border border-danger py-2 mt-1">
+    <div class="col-2">Volunteer Shifts</div>
+    <div class="col-10" id="shifts" name="info" style="color:#ffae00" colspan="3"><?= $record->shifts ?></div>
+  </div>
+</div>
