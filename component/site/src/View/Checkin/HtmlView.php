c451612b4e15ced3b740716c0cce45f39c553d25
muddygs <muddygs@gmail.com>
2025-02-06 20:34:48 -0500

    Refactor: clean up view-handling and authentication logic

--- component/site/src/View/Checkin/HtmlView.php
+++ component/site/src/View/Checkin/HtmlView.php
@@ -18,12 +18,13 @@ use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
 use ClawCorpLib\Lib\Aliases;
 use ClawCorpLib\Lib\EventConfig;
 use ClawCorpLib\Lib\Jwtwrapper;
+use Joomla\CMS\Router\Route;
 
 /** @package ClawCorp\Component\Claw\Site\Controller */
 class HtmlView extends BaseHtmlView
 {
   /**
-   * Execute and display a template script.
+   * Checkin options for badge check, badge print, or meals
    *
    * @param   string  $tpl  The name of the template file to parse; automatically searches through the template paths.
    *
@@ -32,13 +33,14 @@ class HtmlView extends BaseHtmlView
   public function display($tpl = null)
   {
     $this->state = $this->get('State');
+    $tpl = null;
 
+    /** @var \Joomla\CMS\Application\SiteApplication */
     $app = Factory::getApplication();
-    $this->token = $app->input->get('token', '', 'STRING');
+    $this->token = $app->input->get('token', '');
 
     if ($this->token != '') {
-      $nonce = Jwtwrapper::getNonce();
-      $jwt = new Jwtwrapper($nonce);
+      $jwt = new Jwtwrapper();
       $payload = $jwt->confirmToken($this->token, JwtStates::issued);
 
       if ($payload && property_exists($payload, 'state') && array_key_exists($payload->subject, Jwtwrapper::jwt_token_pages)) {
@@ -46,44 +48,53 @@ class HtmlView extends BaseHtmlView
       }
     }
 
-    // If the user is super admin, allow database controls
-    $user = $app->getIdentity();
     $eventConfig = new EventConfig(Aliases::current(true));
 
-    if ($user->authorise('core.admin')) {
-      $this->state->set('user.admin', true);
-      $this->records = Jwtwrapper::getJwtRecords();
-    }
+    switch ($tpl) {
+      case 'meals-checkin':
+        $this->loadMealData($eventConfig);
+        break;
 
-    // Prepare data for meals checkin
-    if ('meals-checkin' == $tpl) {
-      // Categories of interest
-      // TODO: get the category titles from EB
-      $mealHeadings = [
-        $eventConfig->eventInfo->eb_cat_dinners => 'Leather Family Dinner',
-        $eventConfig->eventInfo->eb_cat_buffets => 'Buffets',
-        $eventConfig->eventInfo->eb_cat_brunches => 'Brunches'
-      ];
-
-      $this->meals = [];
-
-      # TODO: could process to eliminate past events
-      foreach ($mealHeadings as $catId => $desc) {
-        $this->meals[-$catId] = $desc;
-        /** @var \ClawCorpLib\Lib\PackageInfo */
-        foreach ($eventConfig->packageInfos as $e) {
-          if ($e->category == $catId) {
-            $this->meals[$e->eventId] = '- ' . $e->title;
-          }
+      case 'badge-print':
+        if ($eventConfig->eventInfo->badgePrintingOverride) {
+          $tpl = 'badges_disabled';
         }
-      }
-    }
+        break;
 
-    if ('badge-print' == $tpl && $eventConfig->eventInfo->badgePrintingOverride) {
-      $tpl = 'badges_disabled';
+      case 'badge-checkin':
+        break;
+
+      default:
+        $route = Route::_('index.php?option=com_claw&view=jwt');
+        $app->redirect($route);
+        break;
     }
 
     $this->setLayout($tpl); // no "default_" prefix
     parent::display();
+  }
+
+  private function loadMealData(EventConfig $eventConfig)
+  {
+    // Categories of interest
+    // TODO: get the category titles from EB
+    $mealHeadings = [
+      $eventConfig->eventInfo->eb_cat_dinners => 'Leather Family Dinner',
+      $eventConfig->eventInfo->eb_cat_buffets => 'Buffets',
+      $eventConfig->eventInfo->eb_cat_brunches => 'Brunches'
+    ];
+
+    $this->meals = [];
+
+    # TODO: could process to eliminate past events
+    foreach ($mealHeadings as $catId => $desc) {
+      $this->meals[-$catId] = $desc;
+      /** @var \ClawCorpLib\Lib\PackageInfo */
+      foreach ($eventConfig->packageInfos as $e) {
+        if ($e->category == $catId) {
+          $this->meals[$e->eventId] = '- ' . $e->title;
+        }
+      }
+    }
   }
 }
