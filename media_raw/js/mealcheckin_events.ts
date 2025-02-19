0b774f112a137c1892f990ac03d8dca8a325a14b
muddygs <muddygs@gmail.com>
2025-02-18 19:52:48 -0500

    Refactor: deprecated most of this to HTMX refactor

--- media_raw/js/mealcheckin_events.ts
+++ media_raw/js/mealcheckin_events.ts
@@ -1,59 +1,11 @@
-let token = "";
-
 window.addEventListener('keydown', function (e) {
   if (e.key == 'Enter') {
     e.preventDefault(); return false;
   }
 }, true);
 
-document.addEventListener("DOMContentLoaded", function () {
-  token = (document.getElementById('token') as HTMLInputElement).value;
-});
-
-function mealsAjaxUrl(task: string) {
-  return `/index.php?option=com_claw&task=checkin.${task}&format=raw`;
-}
-
-function mealsOptions(data: object) {
-  return {
-    method: 'POST',
-    body: JSON.stringify(data),
-    headers: {
-      'Content-Type': 'application/json',
-      'X-CSRF-Token': Joomla.getOptions('csrf.token')
-    }
-  }
-}
-
-function doMealCheckin() {
-  const registration_code = (document.getElementById('badgecode') as HTMLInputElement).value;
-  if (registration_code == null || registration_code === '') return;
-
-  const mealEvent = (document.getElementById('mealEvent') as HTMLSelectElement).value;
-  if (mealEvent == null || mealEvent === '') return;
-
-  const data = {
-    registration_code: registration_code,
-    mealEvent: mealEvent,
-    token: token
-  };
-
-  fetch(mealsAjaxUrl('mealCheckin'), mealsOptions(data))
-    .then(result => result.json())
-    .then(response => {
-      document.getElementById('status').innerHTML = response.message;
-      const badgeInput = document.getElementById('badgecode') as HTMLInputElement;
-
-      if ( response.state && response.state !== 'error' ) {
-        if (badgeInput && response.badge) {
-          badgeInput.value = response.badge;
-        }
-      }
-    }).catch(error => {
-      console.log("Fetch error in doMealCheckin");
-    })
-}
-
 function clearcode() {
-  (document.getElementById('badgecode') as HTMLInputElement).value = '';
+  const badgecode = document.getElementById('badgecode') as HTMLInputElement;
+  badgecode.value = '';
+  badgecode.focus();
 }
