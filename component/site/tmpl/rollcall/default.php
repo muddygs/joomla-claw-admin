a2fdeda57a1a6ec397a83a0ed2df574a8e74f3d3
muddygs <muddygs@gmail.com>
2025-03-06 20:13:10 -0500

    Chore: added (c) header

--- component/site/tmpl/rollcall/default.php
+++ component/site/tmpl/rollcall/default.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * @package     ClawCorp
+ * @subpackage  com_claw
+ *
+ * @copyright   (C) 2025 C.L.A.W. Corp. All Rights Reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
 \defined('_JEXEC') or die;
 
 use Joomla\CMS\Factory;
