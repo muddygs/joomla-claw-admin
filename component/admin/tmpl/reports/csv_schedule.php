930e3bf571d68083848ade4bc65579800ea117be
muddygs <muddygs@gmail.com>
2025-09-20 14:48:15 -0400

    feat: added debugging mode, kinda dangerous, but use will be policy based

--- component/admin/tmpl/reports/csv_schedule.php
+++ component/admin/tmpl/reports/csv_schedule.php
@@ -4,17 +4,18 @@
  * @package     ClawCorp
  * @subpackage  com_claw
  *
- * @copyright   (C) 2023 C.L.A.W. Corp. All Rights Reserved.
+ * @copyright   (C) 2025 C.L.A.W. Corp. All Rights Reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 // No direct access to this file
 \defined('_JEXEC') or die('Restricted Access');
 
-use ClawCorpLib\Helpers\Schedule;
+use ClawCorpLib\Helpers\ScheduleExport;
 use ClawCorpLib\Lib\Aliases;
+use ClawCorpLib\Lib\EventInfo;
 use Joomla\CMS\HTML\HTMLHelper;
 
-$schedule = new Schedule(Aliases::current(true), $this->db);
+$eventInfo = new EventInfo(Aliases::current(true));
 $filename = 'Schedule_Export_' . HtmlHelper::date('now', 'Y-m-d_H-i-s') . '.csv';
-$schedule->toCSV($filename);
+ScheduleExport::toCSV($eventInfo, $filename);
